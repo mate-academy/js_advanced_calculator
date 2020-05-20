665bcd74c295a5151156315ad961ed47d751d2de
DenysCheporniuk <denys.cheporniuk@gmail.com>
2020-05-20 10:58:24 +0300

    added task initial code

---
+++ src/makeCalculator.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * Another calculator. Now the task is more difficult.
+ * Create a `makeCalculator` function that returns an object that
+ * has the following fields:
+ *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
+ *  - The `result` property is initially 0.
+ *
+ * How the calculator will work:
+ * - Each `operate` call takes a number and a callback and making the
+ *   appropriate action with `result`.
+ * - The `reset `method will reset the `result` value to 0.
+ * - The `operate` and `reset` methods can be used in chained calls.
+ *
+ * Example:
+ * const calculator = makeCalculator();
+ *
+ * calculator.operate(calculator.add, 21)
+ * calculator.result === 21
+
+ * calculator.reset()
+ * calculator.result === 0
+
+ * calculator
+ *  .operate(calculator.add, 2)
+ *  .operate(calculator.multiply, 4)
+ *  .reset()
+ *  .operate(calculator.subtract, 5)
+ *  .operate(calculator.multiply, 4)
+ *
+ * calculator.result === -20
+ *
+ *
+ * @return {object}
+ */
+function makeCalculator() {
+  // write code here
+}
+
+module.exports = makeCalculator;
---
+++ src/makeCalculator.test.js
@@ -0,0 +1,174 @@
+'use strict';
+/* global require, describe, test, expect */
+
+const makeCalculator = require('./makeCalculator');
+
+describe('sum', () => {
+  test(`Function 'makeCalculator' should be declared`, () => {
+    expect(makeCalculator).toBeInstanceOf(Function);
+  });
+
+  test(`Method 'result' should return result of type 'number'`, () => {
+    const calculator = makeCalculator();
+
+    expect(typeof calculator.result).toBe('number');
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator.operate(calculator.add, 10);
+        expected output: calculator.result === 10`, () => {
+    const calculator = makeCalculator();
+
+    calculator.operate(calculator.add, 10);
+
+    expect(calculator.result).toBe(10);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator.operate(calculator.subtract, 100);
+        expected output: calculator.result === -100`, () => {
+    const calculator = makeCalculator();
+
+    calculator.operate(calculator.subtract, 100);
+
+    expect(calculator.result).toBe(-100);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.add, 8)
+                .operate(calculator.multiply, 8);
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.add, 8)
+      .operate(calculator.multiply, 8);
+
+    expect(calculator.result).toBe(64);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.add, 27)
+                .operate(calculator.devide, 3);
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.add, 27)
+      .operate(calculator.devide, 3);
+
+    expect(calculator.result).toBe(9);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.add, 3)
+                .operate(calculator.add, 20)
+                .operate(calculator.subtract, 9);
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.add, 3)
+      .operate(calculator.add, 20)
+      .operate(calculator.subtract, 9);
+
+    expect(calculator.result).toBe(14);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.add, 3)
+                .operate(calculator.add, 20)
+                .reset()
+                .operate(calculator.subtract, 9);
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.add, 3)
+      .operate(calculator.add, 20)
+      .reset()
+      .operate(calculator.subtract, 9);
+
+    expect(calculator.result).toBe(-9);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.add, 2)
+                .operate(calculator.multiply, 4)
+                .reset()
+                .operate(calculator.subtract, 5)
+                .operate(calculator.multiply, 4)
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.add, 2)
+      .operate(calculator.multiply, 4)
+      .reset()
+      .operate(calculator.subtract, 5)
+      .operate(calculator.multiply, 4);
+
+    expect(calculator.result).toBe(-20);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.add, 2)
+                .operate(calculator.multiply, 4)
+                .operate(calculator.subtract, 5)
+                .operate(calculator.multiply, 4)
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.add, 2)
+      .operate(calculator.multiply, 4)
+      .operate(calculator.subtract, 5)
+      .operate(calculator.multiply, 4);
+
+    expect(calculator.result).toBe(12);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.subtract, -100)
+                .operate(calculator.multiply, 4)
+                .operate(calculator.add, 2)
+                .operate(calculator.devide, 2)
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.subtract, -100)
+      .operate(calculator.multiply, 4)
+      .operate(calculator.add, 2)
+      .operate(calculator.devide, 2);
+
+    expect(calculator.result).toBe(201);
+  });
+
+  test(`input: const calculator = makeCalculator();
+               calculator
+                .operate(calculator.subtract, -100)
+                .operate(calculator.multiply, 4)
+                .reset()
+                .operate(calculator.add, 2)
+                .operate(calculator.devide, 2)
+        expected output: calculator.result === 64`, () => {
+    const calculator = makeCalculator();
+
+    calculator
+      .operate(calculator.subtract, -100)
+      .operate(calculator.multiply, 4)
+      .reset()
+      .operate(calculator.add, 2)
+      .operate(calculator.devide, 2);
+
+    expect(calculator.result).toBe(1);
+  });
+});
