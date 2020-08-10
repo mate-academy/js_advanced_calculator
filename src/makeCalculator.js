3035b11acb2608760637ea5b15fa6950bf85d23e
DmJSCourse <dimonua@gmail.com>
2020-08-10 15:06:41 +0300

    'this' is fine

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,47 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(number) {
+      this.result += number;
+
+      return this.result;
+    },
+
+    subtract(number) {
+      this.result -= number;
+
+      return this.result;
+    },
+
+    multiply(number) {
+      this.result *= number;
+
+      return this.result;
+    },
+
+    divide(number) {
+      this.result /= number;
+
+      return this.result;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      const call = callback.bind(this);
+
+      call(number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
