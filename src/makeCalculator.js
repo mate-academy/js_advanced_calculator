615b547ac927d4953e2938b60adf1a4e02a9d274
Andrii Hrushetskii <andrii.hrushetskii@gmail.com>
2020-08-10 14:48:33 +0300

    yuuuuuuhuu Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  // creation of our hero
+  const internalCalculator = {
+    result: 0,
+
+    operate: function(operation, number) {
+      operation(number);
+
+      return internalCalculator;
+    },
+
+    reset: function() {
+      internalCalculator.result = 0;
+
+      return internalCalculator;
+    },
+
+    add: function(addition) {
+      internalCalculator.result += addition;
+    },
+
+    subtract: function(substractor) {
+      internalCalculator.result -= substractor;
+    },
+
+    multiply: function(multiplier) {
+      internalCalculator.result *= multiplier;
+    },
+
+    divide: function(divider) {
+      internalCalculator.result /= divider;
+    },
+  };
+
+  // when all done
+  return internalCalculator;
 }
 
 module.exports = makeCalculator;
