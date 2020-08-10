7cee873df4f16fbffabb5954f12f93e8aab8804e
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-10 17:27:59 +0300

    attempt

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,45 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const obj = {
+    result: 0,
+  };
+
+  obj.operate = (action, item) => {
+    return action(item);
+  };
+
+  obj.add = (item) => {
+    obj.result += item;
+
+    return obj;
+  };
+
+  obj.subtract = (item) => {
+    obj.result -= item;
+
+    return obj;
+  };
+
+  obj.multiply = (item) => {
+    obj.result *= item;
+
+    return obj;
+  };
+
+  obj.divide = (item) => {
+    obj.result /= item;
+
+    return obj;
+  };
+
+  obj.reset = () => {
+    obj.result = 0;
+
+    return obj;
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
