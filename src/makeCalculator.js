e2a32e72a7e48e889f129c692d752e2b50d8f25e
uni-8 <julia.zhol.a@gmail.com>
2020-08-10 19:28:26 +0300

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calc = {
+    result: 0,
+
+    reset() {
+      calc.result = 0;
+
+      return calc;
+    },
+
+    operate(callback, value) {
+      callback(value);
+
+      return calc;
+    },
+
+    add(value) {
+      calc.result += value;
+    },
+
+    subtract(value) {
+      calc.result -= value;
+    },
+
+    multiply(value) {
+      calc.result *= value;
+    },
+
+    divide(value) {
+      calc.result /= value;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
