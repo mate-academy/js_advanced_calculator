28c2406e08180aba879b80300a6c0765839b3549
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-14 16:22:06 +0300

    done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calc = {
+    result: 0,
+
+    operate(operator, num) {
+      calc.result = operator(num);
+
+      return calc;
+    },
+
+    add(num) {
+      calc.result += num;
+
+      return calc.result;
+    },
+    subtract(num) {
+      calc.result -= num;
+
+      return calc.result;
+    },
+    divide(num) {
+      calc.result /= num;
+
+      return calc.result;
+    },
+    multiply(num) {
+      calc.result *= num;
+
+      return calc.result;
+    },
+    reset() {
+      calc.result = 0;
+
+      return calc;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
