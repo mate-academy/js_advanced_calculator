c907b43db67a334b1312b1aceafa4274c73f437b
dmitry <akulinind@gmail.com>
2020-09-16 15:04:50 +0300

    add calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, value) {
+      callback(value);
+
+      return this;
+    },
+
+    add(x) {
+      calculator.result += x;
+    },
+
+    subtract(x) {
+      calculator.result -= x;
+    },
+
+    divide(x) {
+      calculator.result /= x;
+    },
+
+    multiply(x) {
+      calculator.result *= x;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
