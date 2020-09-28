2f56cbf855b71b4e31e9aa4b83ee9da7f21be096
38063 <xeilen1112@gmail.com>
2020-09-28 16:34:49 +0300

    -solution with call

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+    },
+
+    subtract(value) {
+      this.result -= value;
+    },
+
+    multiply(value) {
+      this.result *= value;
+    },
+
+    divide(value) {
+      if (value !== 0) {
+        this.result /= value;
+      }
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, value) {
+      callback.call(this, value);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
