76531691f48ad623e1e1adf66de09f1391b1f237
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-09-28 18:55:20 +0300

    Solution calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(value) {
+      if (typeof value !== 'string') {
+        calculator.result += value;
+      }
+    },
+    subtract(value) {
+      if (typeof value !== 'string') {
+        calculator.result -= value;
+      }
+    },
+    multiply(value) {
+      if (typeof value !== 'string') {
+        calculator.result *= value;
+      }
+    },
+    divide(value) {
+      if (typeof value !== 'string') {
+        calculator.result /= value;
+      }
+    },
+    reset(value) {
+      if (typeof value !== 'string') {
+        calculator.result = 0;
+
+        return this;
+      }
+    },
+    operate(callback, value) {
+      callback(value);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
