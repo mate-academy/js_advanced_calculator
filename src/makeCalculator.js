e6bddbfcddb8d13973b4caa1bc581e90254a8ca2
Eugene <evolokhin@gmail.com>
2020-09-30 16:02:05 +0300

    done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const mathOperation = {
+    result: 0,
+
+    add: (x) => {
+      mathOperation.result += x;
+    },
+
+    subtract: (x) => {
+      mathOperation.result -= x;
+    },
+
+    multiply: (x) => {
+      mathOperation.result *= x;
+    },
+
+    divide: (x) => {
+      mathOperation.result /= x;
+    },
+
+    reset: (x) => {
+      mathOperation.result = 0;
+
+      return mathOperation;
+    },
+
+    operate(operation, x) {
+      operation(x);
+
+      return mathOperation;
+    },
+  };
+
+  return mathOperation;
 }
 
 module.exports = makeCalculator;
