2197e55a23b34926c9f78a3e2767042998f44394
Lora Levitska <larakad94@gmail.com>
2020-12-15 19:28:40 +0200

    created calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -35,7 +35,31 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(operation, value) {
+      this.result = operation(this.result, value);
+
+      return this;
+    },
+    result: 0,
+  };
 }
 
 module.exports = makeCalculator;
