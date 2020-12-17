bdb1f59674873cbf3ec2f51f76642455889ba35a
Oleksii Spalylo <white.rh42@gmail.com>
2020-12-17 17:27:20 +0200

    Another calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,39 @@
  */
 function makeCalculator() {
   // write code here
+  const calcObj = {
+    result: 0,
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    subtract(result, number) {
+      return result - number;
+    },
+
+    divide(result, number) {
+      return result / number;
+    },
+
+    multiply(result, number) {
+      return result * number;
+    },
+
+    add(result, number) {
+      return result + number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calcObj;
 }
 
 module.exports = makeCalculator;
