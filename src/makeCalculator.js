14fffd38c16fae7dd98a7cb73a33719844a71386
tamara ostapets <tamara.ostapec@gmail.com>
2020-12-18 11:58:29 +0200

    Calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(f, x) {
+      this.result = f(this.result, x);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(x, y) {
+      return x + y;
+    },
+
+    subtract(x, y) {
+      return x - y;
+    },
+
+    divide(x, y) {
+      return x / y;
+    },
+
+    multiply(x, y) {
+      return x * y;
+    },
+  };
 }
 
 module.exports = makeCalculator;
