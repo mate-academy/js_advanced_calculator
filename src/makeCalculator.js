1fd1a1058c5018b7657e106cd7fd772b41143465
Baburin Eugene <58775463+Baburineu@users.noreply.github.com>
2021-01-11 21:18:29 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,43 @@
  */
 function makeCalculator() {
   // write code here
+  let result = 0;
+
+  const calc = {
+    get result() {
+      return result;
+    },
+
+    add(a) {
+      result += a;
+    },
+
+    subtract(a) {
+      result -= a;
+    },
+
+    divide(a) {
+      result /= a;
+    },
+
+    multiply(a) {
+      result *= a;
+    },
+
+    reset() {
+      result *= 0;
+
+      return this;
+    },
+
+    operate(callback, a) {
+      callback(a);
+
+      return this;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
