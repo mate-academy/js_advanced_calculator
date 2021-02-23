c38a4aec475e9492e6c0f371bdf777f59f67c2e2
Eugene Khablenko <eugene.khablenko@gmail.com>
2021-02-23 19:43:06 +0200

    fixes after review

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -40,30 +40,35 @@ function makeCalculator() {
   const calculator = {
     result: 0,
     add(a) {
-      calculator.result = calculator.result + a;
+      calculator.result += a;
 
       return calculator.result;
     },
+
     subtract(a) {
-      calculator.result = calculator.result - a;
+      calculator.result -= a;
 
       return calculator.result;
     },
+
     multiply(a) {
-      calculator.result = calculator.result * a;
+      calculator.result *= a;
 
       return calculator.result;
     },
+
     divide(a) {
-      calculator.result = calculator.result / a;
+      calculator.result /= a;
 
       return calculator.result;
     },
+
     reset() {
       calculator.result = 0;
 
       return this;
     },
+
     operate(callback, x) {
       this.result = callback(x);
 
