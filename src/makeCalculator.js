494bf1ad411c57feb9ca8dc963392c4e50e01a6b
ViPiven <vitalijjpiven777@gmail.com>
2021-04-28 21:36:58 +0300

    'Solution'

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,39 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+
+    operate(callback, value) {
+      callback(value);
+
+      return this;
+    },
+
+    add(value) {
+      calculator.result += value;
+    },
+
+    subtract(value) {
+      calculator.result -= value;
+    },
+
+    multiply(value) {
+      calculator.result *= value;
+    },
+
+    divide(value) {
+      calculator.result /= value;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
