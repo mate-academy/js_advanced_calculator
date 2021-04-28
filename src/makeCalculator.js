6b1fc5d8148fcf804fcd61cc1d03fc4d54de21de
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-04-28 17:41:35 +0300

    ready for check

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, number) {
+      this.result = callback(number);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(number) {
+      calculator.result += number;
+
+      return calculator.result;
+    },
+    subtract(number) {
+      calculator.result -= number;
+
+      return calculator.result;
+    },
+    multiply(number) {
+      calculator.result *= number;
+
+      return calculator.result;
+    },
+    divide(number) {
+      calculator.result /= number;
+
+      return calculator.result;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
