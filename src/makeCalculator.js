a6b43d56bdcfb02298532a0357ef8176fab355d8
Yehor Merzlenko <yehor.merzlenko@gmail.com>
2021-06-30 18:14:28 +0300

    added empty lines

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -39,23 +39,29 @@
 function makeCalculator() {
   const calculator = {
     result: 0,
+
     add(number) {
       calculator.result += number;
     },
+
     subtract(number) {
       calculator.result -= number;
     },
+
     multiply(number) {
       calculator.result *= number;
     },
+
     divide(number) {
       calculator.result /= number;
     },
+
     reset() {
       calculator.result = 0;
 
       return this;
     },
+
     operate(operation, number) {
       operation(number);
 
