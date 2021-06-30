87e5e40f0dfe82ce9acf38d3174f9d26891f4ace
grynkiv0x001 <grynkiv0x001@gmail.com>
2021-06-30 11:18:45 +0300

    Fixes after review

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -56,12 +56,12 @@ function makeCalculator() {
     reset() {
       calculator.result = 0;
 
-      return calculator;
+      return this;
     },
     operate(callback, operand) {
       callback(operand);
 
-      return calculator;
+      return this;
     },
   };
 
