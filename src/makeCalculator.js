fb03176d98b50992507fb9fd296551cf7106045b
Sergei <serheifil@gmail.com>
2021-07-01 12:47:08 +0300

    were added empty lines

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -39,23 +39,29 @@
 function makeCalculator() {
   const calculator = {
     result: 0,
+
     add(number) {
       calculator.result += number;
     },
+
     subtract(number) {
       calculator.result -= number;
     },
+
     multiply(number) {
       calculator.result *= number;
     },
+
     divide(number) {
       calculator.result /= number;
     },
+
     reset() {
       this.result = 0;
 
       return this;
     },
+
     operate(mathOperation, number) {
       mathOperation(number);
 
