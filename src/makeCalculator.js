65c24d1cf925ec88ac01df55cb25bf9a2c5c0326
Stas Pechenyi <spechenuy@gmail.com>
2021-07-01 13:20:34 +0300

    Issues were fiixed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -39,23 +39,29 @@
 function makeCalculator() {
   const calculator = {
     result: 0,
+
     add(value) {
       calculator.result += value;
     },
+
     subtract(value) {
       calculator.result -= value;
     },
+
     multiply(value) {
       calculator.result *= value;
     },
+
     divide(value) {
       calculator.result /= value;
     },
+
     reset() {
       calculator.result = 0;
 
       return this;
     },
+
     operate(callback, value) {
       callback(value);
 
