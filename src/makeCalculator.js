de10bbd28737796d8cc9696cdb34de2c246ac021
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-01 20:54:09 +0300

    done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,12 +5,12 @@
  * Create a `makeCalculator` function that returns an object that
  * has the following fields:
  *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
+ *  - The `this.result` property is initially 0.
  *
  * How the calculator will work:
  * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
+ *   appropriate value to the `this.result` property.
+ * - The `reset` method resets `this.result` value to 0.
  * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
  *   `operate` method
  * - The `operate` and `reset` methods can be called in a chain.
@@ -19,10 +19,10 @@
  * const calculator = makeCalculator();
  *
  * calculator.operate(calculator.add, 21)
- * calculator.result === 21
+ * calculator.this.result === 21
 
  * calculator.reset()
- * calculator.result === 0
+ * calculator.this.result === 0
 
  * calculator
  *  .operate(calculator.add, 10)
@@ -31,13 +31,45 @@
  *  .operate(calculator.divide, 5)
  *  .operate(calculator.multiply, 7)
  *
- * calculator.result === -28
+ * calculator.this.result === -28
  *
  *
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(n) {
+      calculator.result += n;
+    },
+
+    subtract(n) {
+      calculator.result -= n;
+    },
+
+    multiply(n) {
+      calculator.result *= n;
+    },
+
+    divide(n) {
+      calculator.result /= n;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, n) {
+      operation(n);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
