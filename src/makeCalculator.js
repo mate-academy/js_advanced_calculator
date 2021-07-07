886da94ae5da59b321e76a903bdadb304a27d0db
Viktormaz <vitecc91@gmail.com>
2021-07-07 16:10:58 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,12 +5,12 @@
  * Create a `makeCalculator` function that returns an object that
  * has the following fields:
  *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
+ *  - The `calculator.result` property is initially 0.
  *
  * How the calculator will work:
  * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
+ *   appropriate value to the `calculator.result` property.
+ * - The `reset` method resets `calculator.result` value to 0.
  * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
  *   `operate` method
  * - The `operate` and `reset` methods can be called in a chain.
@@ -19,10 +19,10 @@
  * const calculator = makeCalculator();
  *
  * calculator.operate(calculator.add, 21)
- * calculator.result === 21
+ * calculator.calculator.result === 21
 
  * calculator.reset()
- * calculator.result === 0
+ * calculator.calculator.result === 0
 
  * calculator
  *  .operate(calculator.add, 10)
@@ -31,13 +31,55 @@
  *  .operate(calculator.divide, 5)
  *  .operate(calculator.multiply, 7)
  *
- * calculator.result === -28
+ * calculator.calculator.result === -28
  *
  *
  * @return {object}
  */
 function makeCalculator() {
   // write code here
+
+  const calculator = {
+    result: 0,
+
+    add(val) {
+      calculator.result += val;
+
+      return calculator.result;
+    },
+
+    subtract(val) {
+      calculator.result -= val;
+
+      return calculator.result;
+    },
+
+    multiply(val) {
+      calculator.result *= val;
+
+      return calculator.result;
+    },
+
+    divide(val) {
+      calculator.result /= val;
+
+      return calculator.result;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, val) {
+      callback(val);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
