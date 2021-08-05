b763abe9058c55f8de08a4882cec726f17b731fe
Anna-Tkachenko <tkachenko.anna835@gmail.com>
2021-08-05 12:07:38 +0300

    Added arrow functions and exception

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -52,20 +52,15 @@ function makeCalculator() {
       return this;
     },
 
-    add: function(a, b) {
-      return a + b;
-    },
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => {
+      if (b === 0) {
+        throw new Error('Invalid value');
+      }
 
-    subtract: function(a, b) {
-      return a - b;
-    },
-
-    divide: function(a, b) {
       return a / b;
-    },
-
-    multiply: function(a, b) {
-      return a * b;
     },
   };
 
