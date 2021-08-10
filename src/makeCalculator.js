d237be3261d8765ca5a4f9126fac53a1a224f366
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-08-10 16:50:52 +0300

    first commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,31 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add(number1, number2) {
+      return (number1 + number2);
+    },
+    subtract(number1, number2) {
+      return (number1 - number2);
+    },
+    multiply(number1, number2) {
+      return (number1 * number2);
+    },
+    divide(number1, number2) {
+      return (number1 / number2);
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
