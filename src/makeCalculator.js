1601186cf20fbb31d6a8059aae8a91bbb19bcba5
Dima Cherednyk <dicherednyk@gmail.com>
2021-08-25 23:16:07 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,43 +1,57 @@
 'use strict';
 
-/**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
- *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
- *
- * Example:
- * const calculator = makeCalculator();
- *
- * calculator.operate(calculator.add, 21)
- * calculator.result === 21
-
- * calculator.reset()
- * calculator.result === 0
-
- * calculator
- *  .operate(calculator.add, 10)
- *  .reset()
- *  .operate(calculator.subtract, 20)
- *  .operate(calculator.divide, 5)
- *  .operate(calculator.multiply, 7)
- *
- * calculator.result === -28
- *
- *
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
-}
+  const calculator = {
+    result: 0,
+    add: function(y) {
+      this.result = this.result + y;
+    },
+    subtract: function(y) {
+      this.result = this.result - y;
+    },
+    multiply: function(y) {
+      this.result = this.result * y;
+    },
+    divide: function(y) {
+      this.result = this.result / y;
+    },
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function(x, y) {
+      if (x === calculator.add) {
+        this.add(y);
+
+        return this;
+      };
+
+      if (x === calculator.subtract) {
+        this.subtract(y);
+
+        return this;
+      };
+
+      if (x === calculator.multiply) {
+        this.multiply(y);
+
+        return this;
+      };
+
+      if (x === calculator.divide) {
+        this.divide(y);
+
+        return this;
+      };
+
+      if (x === calculator.reset) {
+        this.reset(y);
+      };
+    },
+  };
+
+  return calculator;
+};
 
 module.exports = makeCalculator;
