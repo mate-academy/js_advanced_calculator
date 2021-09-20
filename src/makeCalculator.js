faf777b9f6267bbcbc51d3b32e137e2fd00d9c47
Victor Shevchuk <callmevitok@gmail.com>
2021-09-20 20:14:12 +0300

    https://www.youtube.com/watch?v=_dt7RJGpPhg&list=LL&index=1

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate(operation, value) {
+      operation(value);
+
+      return calculator;
+    },
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+    add(value) {
+      calculator.result += value;
+    },
+    subtract(value) {
+      calculator.result -= value;
+    },
+    multiply(value) {
+      calculator.result *= value;
+    },
+    divide(value) {
+      calculator.result /= value;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
