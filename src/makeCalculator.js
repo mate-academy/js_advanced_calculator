63909cf79701a87cefb1d0c6f9fe9d8915fccc80
VladSaen <qwefghjklgfd@gmail.com>
2021-09-22 09:53:06 +0300

    c

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, number) {
+      callback(number);
+
+      return this;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+
+    add(num) {
+      calculator.result += num;
+    },
+
+    subtract(num) {
+      calculator.result -= num;
+    },
+
+    divide(num) {
+      calculator.result /= num;
+    },
+
+    multiply(num) {
+      calculator.result *= num;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
