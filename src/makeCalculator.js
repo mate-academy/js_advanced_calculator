1ab33539c03a51addb761219daafeb13f7bb5faa
Jana Tretjak <tretiachechka@gmail.com>
2021-10-12 16:59:01 +0200

    ver01

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,40 @@
  *
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
+
+function makeCalculator(operate, num) {
+  const calculator = {
+    result: 0,
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+
+    operate(action, value) {
+      action(value);
+
+      return this;
+    },
+
+    add(value) {
+      calculator.result += value;
+    },
+
+    subtract(value) {
+      calculator.result -= value;
+    },
+
+    divide(value) {
+      calculator.result /= value;
+    },
+
+    multiply(value) {
+      calculator.result *= value;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
