73bf9f1d7736c29276e884b56359d44f66d92766
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2021-11-11 19:27:26 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,50 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(value) {
+      calculator.result += value;
+    },
+
+    subtract(value) {
+      calculator.result -= value;
+    },
+
+    divide(value) {
+      calculator.result /= value;
+    },
+
+    multiply(value) {
+      calculator.result *= value;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+
+    operate(callbackFunc, value) {
+      callbackFunc(value);
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
+
+// operate(callback, num) {
+//   callback(num);
+
+//   return calculator;
+// },
+// reset() {
+//   calculator.result = 0;
+
+//   return calculator;
+// },
 
 module.exports = makeCalculator;
