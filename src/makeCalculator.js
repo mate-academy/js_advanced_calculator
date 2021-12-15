51e341bcbc6259de4a20a1b45f9f3a0f0c1c7a99
KHTRE <KHTREE@gmail.com>
2021-12-15 16:39:04 +0200

    arrow function

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -40,21 +40,13 @@ function makeCalculator() {
   const obj = {
     result: 0,
 
-    add(a, b) {
-      return a + b;
-    },
+    add: (a, b) => a + b,
 
-    subtract(a, b) {
-      return a - b;
-    },
+    subtract: (a, b) => a - b,
 
-    multiply(a, b) {
-      return a * b;
-    },
+    multiply: (a, b) => a * b,
 
-    divide(a, b) {
-      return a / b;
-    },
+    divide: (a, b) => a / b,
 
     reset() {
       this.result = 0;
