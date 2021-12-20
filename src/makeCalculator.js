6f47073c3bba09f78564f94ad617199b2054f81e
levix2l <alexleviz13@gmail.com>
2021-12-20 20:11:49 +0200

    advanced calculator solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(value) {
+      calculator.result += value;
+    },
+
+    subtract(value) {
+      calculator.result -= value;
+    },
+
+    divide(value) {
+      calculator.result /= value;
+    },
+
+    multiply(value) {
+      calculator.result *= value;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+
+    operate(callback, num) {
+      callback(num);
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
