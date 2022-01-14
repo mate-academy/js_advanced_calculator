6fb70bf78884c131b8ff21ff0c28e5394b7b6835
ov3rst33r <wattsw619@gmail.com>
2022-01-14 15:29:16 +0200

    v1

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,45 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(x) {
+      this.result += x;
+
+      return this;
+    },
+
+    subtract(x) {
+      this.result -= x;
+
+      return this;
+    },
+
+    multiply(x) {
+      this.result *= x;
+
+      return this;
+    },
+
+    divide(x) {
+      this.result /= x;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(func, value) {
+      func.call(this, value);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
