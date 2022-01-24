b284592eaba5a33320e11bc2891d453ff912e793
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-01-24 17:51:44 +0300

    calc

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,37 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    add(value, number) {
+      return value + number;
+    },
+
+    subtract(value, number) {
+      return value - number;
+    },
+
+    multiply(value, number) {
+      return value * number;
+    },
+
+    divide(value, number) {
+      return value / number;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
