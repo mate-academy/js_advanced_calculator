76258b3aa7954f7203623a5a053b9331775555a7
Yurii Tyslenok <tislenok91@gmail.com>
2022-02-12 13:17:38 +0200

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,58 +1,8 @@
 'use strict';
 
-/**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
- *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
- *
- * Example:
- * const calculator = makeCalculator();
- *
- * calculator.operate(calculator.add, 21)
- * calculator.result === 21
-
- * calculator.reset()
- * calculator.result === 0
-
- * calculator
- *  .operate(calculator.add, 10)
- *  .reset()
- *  .operate(calculator.subtract, 20)
- *  .operate(calculator.divide, 5)
- *  .operate(calculator.multiply, 7)
- *
- * calculator.result === -28
- *
- *
- * @return {object}
- */
-
 function makeCalculator() {
-/*   let result = 0; */
-  const calculator = {
+  return {
     result: 0,
-
-    operate(callback, value) {
-      calculator.result = callback(calculator.result, value);
-
-      return calculator;
-    },
-
-    reset() {
-      calculator.result = 0;
-
-      return calculator;
-    },
 
     add(a, b) {
       return a + b;
@@ -69,9 +19,19 @@ function makeCalculator() {
     divide(a, b) {
       return a / b;
     },
-  };
 
-  return calculator;
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
