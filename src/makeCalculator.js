eb84db9b4af2f3c2be64dc4320952d813dd836e8
TJgkOsVP <sasha06052001@gmail.com>
2022-04-04 22:34:48 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -41,26 +41,20 @@ function makeCalculator() {
     result: 0,
     add: (a) => {
       calculator.result += a;
-
-      return calculator;
     },
     subtract: (a) => {
       calculator.result -= a;
-
-      return calculator;
     },
     multiply: (a) => {
       calculator.result *= a;
-
-      return calculator;
     },
     divide: (a) => {
       calculator.result /= a;
-
-      return calculator;
     },
     operate: (callback, number) => {
-      return callback(number);
+      callback(number);
+
+      return calculator;
     },
     reset: () => {
       calculator.result = 0;
