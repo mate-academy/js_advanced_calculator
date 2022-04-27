2f9becd003eb7ada722d7cef52568214ac9f7760
efdalyalcin <efdalyalcin@hotmail.com>
2022-04-27 22:56:23 +0300

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,45 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    calc: 0,
+
+    get result() {
+      return this.calc;
+    },
+    // get result is created since checklist says not to use result var
+
+    operate(callback, num) {
+      callback(num);
+
+      return this;
+    },
+
+    // I have no idea why this doesn't work in add, subtract etc.
+    add(value) {
+      calculator.calc += value;
+    },
+
+    subtract(value) {
+      calculator.calc -= value;
+    },
+
+    multiply(value) {
+      calculator.calc *= value;
+    },
+
+    divide(value) {
+      calculator.calc /= value;
+    },
+
+    reset() {
+      calculator.calc = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
