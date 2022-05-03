8d902f049952d26dec7c3a8a7a5ad867916f3454
Andrei Sucman <sucmanandrei@gmail.com>
2022-05-03 16:54:17 +0300

    task completed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(value = 0) {
+      calculator.result += value;
+    },
+
+    subtract(value = 0) {
+      calculator.result -= value;
+    },
+
+    multiply(value = 1) {
+      calculator.result *= value;
+    },
+
+    divide(value = 1) {
+      calculator.result /= value;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+
+    operate(callback, value) {
+      callback(value);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
