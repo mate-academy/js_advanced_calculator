bf0994701905184a3a5aa700a13d800a4690833c
Artem Furdela <furdela.artem@gmail.com>
2022-05-26 21:45:43 +0300

    Advanced calculator first commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,38 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+    add(value) {
+      calculator.result += value;
+    },
+
+    subtract(value) {
+      calculator.result -= value;
+    },
+
+    multiply(value) {
+      calculator.result *= value;
+    },
+
+    divide(value) {
+      calculator.result /= value;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+
+    operate(operation, value) {
+      operation(value);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
