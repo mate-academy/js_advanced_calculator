fbfa025ab42f9d04582b7be5d9fa4fd7e01ac80f
Julia Makarenko <julys.makarenko@gmail.com>
2022-06-14 13:30:41 +0300

    Add js advanced calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,26 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
