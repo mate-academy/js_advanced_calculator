f7e3cea82979a5ba2d7159c4303fb581342b843a
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-17 00:27:18 +0300

    first commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,36 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+    operate(callback, number) {
+      this.result = callback.call(this, number);
+
+      return this;
+    },
+    add(number) {
+      return this.result + number;
+    },
+
+    subtract(number) {
+      return this.result - number;
+    },
+
+    multiply(number) {
+      return this.result * number;
+    },
+    divide(number) {
+      return this.result / number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
