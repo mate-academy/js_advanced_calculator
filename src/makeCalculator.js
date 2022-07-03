b9584995ad6dc001bfc053055c68c42729b18398
abobyr <artem.bobyr@dataart.com>
2022-07-03 16:19:21 +0300

    make task ready

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    add,
+    subtract,
+    multiply,
+    divide,
+    reset,
+    operate,
+    result: 0,
+  };
+}
+
+function operate(callback, number) {
+  callback(this, number);
+
+  return this;
+}
+
+function add(object, number) {
+  object.result += number;
+}
+
+function subtract(object, number) {
+  object.result -= number;
+}
+
+function multiply(object, number) {
+  object.result *= number;
+}
+
+function divide(object, number) {
+  object.result /= number;
+}
+
+function reset() {
+  this.result = 0;
+
+  return this;
 }
 
 module.exports = makeCalculator;
