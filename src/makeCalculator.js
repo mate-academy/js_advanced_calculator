8ef016eb810701f06aadc730f93e9a8b24f679c2
Jamal Amoian <leonidagutin09@gmail.com>
2022-08-13 17:45:29 +0300

    Calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, number) {
+      callback(number);
+
+      return this;
+    },
+
+    add(operand) {
+      calculator.result += operand;
+    },
+
+    divide(operand) {
+      calculator.result /= operand;
+    },
+
+    subtract(operand) {
+      calculator.result -= operand;
+    },
+
+    multiply(operand) {
+      calculator.result *= operand;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
