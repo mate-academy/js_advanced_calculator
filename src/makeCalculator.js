4f7574a665037ed35f544d9e6184ceb64a86da19
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2022-08-16 15:58:26 +0300

    done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,42 @@
  *
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  const calculator = {
+
+    result: 0,
+
+    add(num) {
+      this.result += num;
+    },
+
+    subtract(num) {
+      this.result -= num;
+    },
+
+    multiply(num) {
+      this.result *= num;
+    },
+
+    divide(num) {
+      this.result /= num;
+    },
+
+    reset(num) {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, num) {
+      callback.apply(this, [num]);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
