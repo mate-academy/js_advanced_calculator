b69d70550ebed173760f49e45b6c74cd9a631a32
Dima <ddikey1@gmail.com>
2022-09-18 01:12:17 +0300

    Solution of

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,44 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  // object created
+  const calculator = {
+    result: 0,
+
+    // created callback operator methods
+    add: function(prevValue, number) {
+      return prevValue + number;
+    },
+
+    subtract(prevValue, number) {
+      return prevValue - number;
+    },
+
+    multiply(prevValue, number) {
+      return prevValue * number;
+    },
+
+    divide(prevValue, number) {
+      return prevValue / number;
+    },
+
+    // created operate method wich take above methods as callback and
+    // number which will be operate.
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    // created `reset` method which resets `result` value to 0.
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
