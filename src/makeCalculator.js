80f5d9884df90a86b536036e413c693f361cd0a8
Volodymyr Klymkiv <volodymyr.klymkiv@gmail.com>
2022-09-18 21:16:29 +0300

    solution js task calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,39 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    add(value, number) {
+      return value + number;
+    },
+
+    subtract(value, number) {
+      return value - number;
+    },
+
+    divide(value, number) {
+      return value / number;
+    },
+
+    multiply(value, number) {
+      return value * number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
