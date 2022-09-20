62b06a770db50861c0ed559aea566d65e9833370
Denys Kleimenov <denyskleimenovit@gmail.com>
2022-09-20 16:39:08 +0300

    Calculator was created

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(firstNumber, secondNumber) {
+      return firstNumber + secondNumber;
+    },
+
+    subtract(firstNumber, secondNumber) {
+      return firstNumber - secondNumber;
+    },
+
+    divide(firstNumber, secondNumber) {
+      return firstNumber / secondNumber;
+    },
+
+    multiply(firstNumber, secondNumber) {
+      return firstNumber * secondNumber;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
