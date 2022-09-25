01f706b2c180f1a3b2ae3b122da0741f89ca4805
Dmytro Yankovskyi <yankovdimon@gmail.com>
2022-09-25 20:03:05 +0300

    done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,35 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(value) {
+      calculator.result += value;
+    },
+    subtract(value) {
+      calculator.result -= value;
+    },
+    multiply(value) {
+      calculator.result *= value;
+    },
+    divide(value) {
+      calculator.result /= value;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, argument) {
+      operation(argument);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
