6dd617f21bce836033801ed217ffc7e6759180ce
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2022-10-16 14:18:30 +0200

    commit message

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -40,43 +40,49 @@ function makeCalculator() {
   // write code here
   const calculator = {
     result: 0,
-
-    reset() {
-      calculator.result = 0;
-
-      return calculator;
-    },
-
-    add(number) {
-      calculator.result += number;
-
-      return calculator.result;
-    },
-
-    subtract(number) {
-      calculator.result -= number;
-
-      return calculator.result;
-    },
-
-    multiply(number) {
-      calculator.result *= number;
-
-      return calculator.result;
-    },
-
-    divide(number) {
-      calculator.result /= number;
-
-      return calculator.result;
-    },
-
-    operate(callback, number) {
-      calculator.result = callback(number);
-
-      return calculator;
-    },
   };
+
+  function reset() {
+    this.result *= 0;
+
+    return calculator;
+  };
+  calculator.reset = reset.bind(calculator);
+
+  function add(number) {
+    this.result += number;
+
+    return this.result;
+  };
+  calculator.add = add.bind(calculator);
+
+  function subtract(number) {
+    this.result -= number;
+
+    return this.result;
+  };
+  calculator.subtract = subtract.bind(calculator);
+
+  function multiply(number) {
+    this.result *= number;
+
+    return this.result;
+  };
+  calculator.multiply = multiply.bind(calculator);
+
+  function divide(number) {
+    this.result /= number;
+
+    return this.result;
+  };
+  calculator.divide = divide.bind(calculator);
+
+  function operate(callback, number) {
+    callback.call(this, number);
+
+    return this;
+  };
+  calculator.operate = operate.bind(calculator);
 
   return calculator;
 }
