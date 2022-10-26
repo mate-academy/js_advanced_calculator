2ad9f47e14ffd4b58dedb4ad011a3bf8f8b1e87f
Svitlana Yudina <osbka31@gmail.com>
2022-10-26 17:42:46 +0300

    calculator solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    divide(a, b) {
+      if (b === 0) {
+        throw new Error('Can\'t divide by zero');
+      }
+
+      return a / b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(action, number) {
+      this.result = action(this.result, number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
