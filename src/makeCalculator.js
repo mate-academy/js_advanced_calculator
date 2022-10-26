4ba82f7f6d22a44b1d4e30eb9c2c5c12646828cb
Iryna Lelyk <irunochka771@gmail.com>
2022-10-26 13:47:56 +0300

    Calculator was created

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(num1, num2) {
+      return num1 + num2;
+    },
+
+    subtract(num1, num2) {
+      return num1 - num2;
+    },
+
+    multiply(num1, num2) {
+      return num1 * num2;
+    },
+
+    divide(num1, num2) {
+      if (num2 === 0) {
+        throw new Error('can\'t divide by zero');
+      }
+
+      return num1 / num2;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
