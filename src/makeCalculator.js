ebb394ee34f363e683dae24386a2a43ed4185fb8
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2022-10-27 11:01:55 +0300

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    add(num1, num2) {
+      return num1 + num2;
+    },
+
+    subtract(num1, num2) {
+      return num1 - num2;
+    },
+
+    multiply(num1, num2) {
+      return num1 * num2;
+    },
+
+    divide(num1, num2) {
+      if (num1 !== 0 && num2 !== 0) {
+        return num1 / num2;
+      } else {
+        throw new Error('Insert a non-0 value');
+      }
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
