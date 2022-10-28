35df3e399f8c940e14557d611edac6eee795a6fe
mitattt <garkavivii@gmail.com>
2022-10-28 21:55:24 +0300

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    add(result, number) {
+      return result + number;
+    },
+
+    subtract(result, number) {
+      return result - number;
+    },
+
+    divide(result, number) {
+      if (number === 0) {
+        throw new Error('You can not divide by 0!');
+      }
+
+      return result / number;
+    },
+
+    multiply(result, number) {
+      return result * number;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
