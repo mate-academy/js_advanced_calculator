7b4957de884cde1757eec6bcc14c2264ca47eb1a
Kukharuk <its.up.to.you.dk@gmail.com>
2022-10-28 15:51:08 +0300

    makeCakcDK

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,38 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, param) {
+      callback.call(this, param);
+
+      return this;
+    },
+    add(val) {
+      this.result += val;
+    },
+
+    subtract(val) {
+      this.result -= val;
+    },
+
+    divide(val) {
+      this.result /= val;
+    },
+
+    multiply(val) {
+      this.result *= val;
+    },
+
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
