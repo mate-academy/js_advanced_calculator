28b6ce3d62ddeda4195e44d1329707ab03019045
Mia Biletska <solomiya100506@gmail.com>
2022-10-31 18:21:53 +0200

    advanced calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,42 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, num) {
+      this.result = callback(this.result, num);
+
+      return this;
+    },
+    add(currentResult, num) {
+      return currentResult + num;
+    },
+
+    subtract(currentResult, num) {
+      return currentResult - num;
+    },
+
+    multiply(currentResult, num) {
+      return currentResult * num;
+    },
+
+    divide(currentResult, num) {
+      if (num === 0) {
+        throw new Error(`Can't divide by zero`);
+      }
+
+      return currentResult / num;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
