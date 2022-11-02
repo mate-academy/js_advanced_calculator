1f0bb0b42cd115fee3b01d10e044fdfbb60a02c2
Sasha Kornieiev <doktoralex8808@gmail.com>
2022-11-02 23:37:30 +0200

    calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,42 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
-}
+  const calculator = {
+    result: 0,
 
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    add(num1, num2) {
+      return num1 + num2;
+    },
+
+    subtract(num1, num2) {
+      return num1 - num2;
+    },
+
+    multiply(num1, num2) {
+      return num1 * num2;
+    },
+
+    divide(num1, num2) {
+      if (num2 === 0) {
+        throw new Error('Divide by zero is not allowed!');
+      } else {
+        return num1 / num2;
+      }
+    },
+  };
+
+  return calculator;
+}
 module.exports = makeCalculator;
