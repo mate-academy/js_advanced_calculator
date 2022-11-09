d2802f88e770d7ccbc1baef8ec065f18c387db24
zbyn3k <coding707@mailbox.org>
2022-11-09 10:13:28 +0100

    Added the solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,4 +1,3 @@
-'use strict';
 
 /**
  * Another calculator. Now the task is more difficult.
@@ -36,8 +35,41 @@
  *
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  const calculatorObject = {
+    result: 0,
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      return a / b;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+  };
+
+  return calculatorObject;
 }
 
 module.exports = makeCalculator;
