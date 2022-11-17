436a25de35eef332f02e06464db559ff8885a23b
Nick Busarhin <busarginkolia@gmail.com>
2022-11-17 18:14:01 +0200

    calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(number) {
+      return this.result + number;
+    },
+    subtract(number) {
+      return this.result - number;
+    },
+    multiply(number) {
+      return this.result * number;
+    },
+    divide(number) {
+      return this.result / number;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(operation, number) {
+      this.result = operation.call(this, number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
