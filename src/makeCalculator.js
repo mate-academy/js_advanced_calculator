cb4b4be9db5f3110fc9362584aae5f4d05b716d1
haduigon <haduigon@gmail.com>
2022-11-17 23:59:49 +0200

    first commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -23,7 +23,6 @@
 
  * calculator.reset()
  * calculator.result === 0
-
  * calculator
  *  .operate(calculator.add, 10)
  *  .reset()
@@ -38,6 +37,39 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+    operate: function(callback, value) {
+      callback(value);
+
+      return this;
+    },
+    add: function(number) {
+      calculator.result += number;
+
+      return this;
+    },
+    subtract: function(number) {
+      calculator.result = calculator.result - Math.abs(number);
+
+      return this;
+    },
+    multiply: function(number) {
+      calculator.result = calculator.result * number;
+    },
+    divide: function(number) {
+      calculator.result = calculator.result / number;
+
+      return this;
+    },
+    reset: function() {
+      calculator.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
