e7e0e804a5319cabf556150ca68c09cb96c6fe26
stetsenko_viktoriia <stetsenko.viktoriia@gmail.com>
2022-11-19 20:25:18 +0200

    fixed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -39,23 +39,29 @@
 function makeCalculator() {
   const calculator = {
     result: 0,
+
     add: (number) => {
       calculator.result += number;
     },
+
     subtract: (number) => {
       calculator.result -= number;
     },
+
     multiply: (number) => {
       calculator.result *= number;
     },
+
     divide: (number) => {
       calculator.result /= number;
     },
+
     reset: () => {
       calculator.result = 0;
 
       return calculator;
     },
+
     operate: (callback, number) => {
       callback(number);
 
