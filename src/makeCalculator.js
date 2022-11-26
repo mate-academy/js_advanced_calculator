798d715c7b711b0ea61b8ae537985648f87c6404
Halyna Rydasova <halyna.rydasova@gmail.com>
2022-11-26 15:51:14 +0200

    Fixed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -40,24 +40,26 @@ function makeCalculator() {
   const calculator = {
     result: 0,
     add(number) {
-      calculator.result += number;
+      this.result += number;
     },
     subtract(number) {
-      calculator.result -= number;
+      this.result -= number;
     },
     divide(number) {
-      calculator.result /= number;
+      this.result /= number;
     },
     multiply(number) {
-      calculator.result *= number;
+      this.result *= number;
     },
     reset() {
-      calculator.result = 0;
+      this.result = 0;
 
       return this;
     },
     operate(callback, number) {
-      callback(number);
+      const bindedcallback = callback.bind(this, number);
+
+      bindedcallback();
 
       return this;
     },
