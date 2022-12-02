53797b82abdfcfdb8a9e88a84aea4e1cab6e459b
Oleh Yablunovskyi <lifserv@gmail.com>
2022-12-02 13:32:26 +0200

    Function logic was redisigned

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -40,6 +40,22 @@ function makeCalculator() {
   const calculator = {
     result: 0,
 
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      return a / b;
+    },
+
     reset() {
       this.result = 0;
 
@@ -47,31 +63,7 @@ function makeCalculator() {
     },
 
     operate(callback, number) {
-      callback(number);
-
-      return this;
-    },
-
-    add(number) {
-      calculator.result += number;
-
-      return this;
-    },
-
-    subtract(number) {
-      calculator.result -= number;
-
-      return this;
-    },
-
-    multiply(number) {
-      calculator.result *= number;
-
-      return this;
-    },
-
-    divide(number) {
-      calculator.result /= number;
+      this.result = callback(this.result, number);
 
       return this;
     },
