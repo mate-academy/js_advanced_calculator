027b693e5ecff55cb059af4c90e01f66bca64373
Kavster92 <kav.athi1992@googlemail.com>
2022-12-02 01:31:13 +0100

    first attempt at Adavnced Calculator, with callback

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,41 @@
  *
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  const calc = {
+    result: 0,
+
+    addition(a, b) {
+      return a + b;
+    },
+
+    subtraction(a, b) {
+      return a - b;
+    },
+
+    division(a, b) {
+      return a / b;
+    },
+
+    multiplication(a, b) {
+      return a * b;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, number) {
+      this.result = operation(this.result, number);
+
+      return this;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
