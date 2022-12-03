fbe8421bc1a5e9f47b378e2684f75e1bfd15913d
Sergei Yurchenko <sergyurch@gmail.com>
2022-12-03 20:53:43 +0200

    Add makeCalculator function

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    divide(a, b) {
+      return a / b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, x) {
+      this.result = callback(this.result, x);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
