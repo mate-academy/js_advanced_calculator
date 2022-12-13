0d9278079b3aebfd3e4a22020a5ffe9018f4f2b2
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2022-12-13 11:43:26 +0200

    commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,32 @@
  *
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
-}
+  function makeCalculator() {
+    let obj = {
+      result: 0,
+      add: function(number) {
+        obj.result = (obj.result + number);
+      },
+      subtract: function(number) {
+        obj.result = (obj.result - number);
+      },
+      multiply: function(number) {
+        obj.result = (obj.result * number);
+      },
+      divide: function(number) {
+        obj.result = (obj.result / number);
+      },
+      reset: function() {
+        obj.result = 0;
+        return this;
+      },
+      operate: function(callback, number) {
+        callback(number);
+        return this;
+      },
+    };
+
+    return obj;
+  }
 
 module.exports = makeCalculator;
