5b2e5da6bfcf7db5d07ff3e14f645b250254256a
svitjojo <kornotavr@gmail.com>
2023-01-03 15:06:19 +0200

    makeCalculator function was created

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate(callback, number) {
+      callback(number);
+
+      return this;
+    },
+    add(num) {
+      calculator.result += num;
+    },
+    subtract(num) {
+      calculator.result -= num;
+    },
+    divide(num) {
+      calculator.result /= num;
+    },
+    multiply(num) {
+      calculator.result *= num;
+    },
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
