6fe12df3ab92b2f144cfc7d76370c6defa913faa
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2023-01-05 21:21:09 +0200

    Fixes after code review

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -41,29 +41,29 @@ function makeCalculator() {
     result: 0,
 
     operate(callback, number) {
-      callback(number);
+      callback.call(this, number);
 
       return calculate;
     },
 
     add(number) {
-      calculate.result += number;
+      this.result += number;
     },
 
     subtract(number) {
-      calculate.result -= number;
+      this.result -= number;
     },
 
     multiply(number) {
-      calculate.result *= number;
+      this.result *= number;
     },
 
     divide(number) {
-      calculate.result /= number;
+      this.result /= number;
     },
 
     reset() {
-      calculate.result = 0;
+      this.result = 0;
 
       return calculate;
     },
