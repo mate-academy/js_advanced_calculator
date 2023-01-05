20354a589b52570a4764d7272db8e2a4b8da8100
Brandon Sunrise <stepansky93@gmail.com>
2023-01-05 16:46:44 +0200

    corrected by checklist

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -40,30 +40,30 @@ function makeCalculator() {
   const calculator = {
     result: 0,
 
-    add(number) {
-      calculator.result += number;
+    add(numberA, numberB) {
+      return numberA + numberB;
     },
 
-    subtract(number) {
-      calculator.result -= number;
+    subtract(numberA, numberB) {
+      return numberA - numberB;
     },
 
-    multiply(number) {
-      calculator.result *= number;
+    multiply(numberA, numberB) {
+      return numberA * numberB;
     },
 
-    divide(number) {
-      calculator.result /= number;
+    divide(numberA, numberB) {
+      return numberA / numberB;
     },
 
-    reset(number) {
-      calculator.result = 0;
+    reset() {
+      this.result = 0;
 
       return this;
     },
 
     operate(callback, number) {
-      callback(number);
+      this.result = callback(this.result, number);
 
       return this;
     },
