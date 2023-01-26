a58fcf79dd02279c3ed39f374e9de7eda069fe62
Artem Kravcov <a.kravcov1985@gmail.com>
2023-01-26 17:37:43 +0200

    Calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,47 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    subtract(value) {
+      calculator.result -= value;
+
+      return this;
+    },
+
+    add(value) {
+      calculator.result += value;
+
+      return this;
+    },
+
+    multiply(value) {
+      calculator.result *= value;
+
+      return this;
+    },
+
+    divide(value) {
+      calculator.result /= value;
+
+      return this;
+    },
+
+    operate(callback, number = 0) {
+      callback(number);
+
+      return this;
+    },
+
+    reset(value) {
+      calculator.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
