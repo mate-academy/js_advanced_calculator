75864d92b5fef546c5043cbcfb725420a7531b07
Andrew Litkovets <andriylao@gmail.com>
2023-01-27 13:26:08 +0200

    Task done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const answer = {
+    result: 0,
+    operate: function(callback, number) {
+      callback(number);
+
+      return answer;
+    },
+    reset: function() {
+      answer.result = 0;
+
+      return answer;
+    },
+    add: function(number) {
+      answer.result += number;
+    },
+    subtract: function(number) {
+      answer.result -= number;
+    },
+    multiply: function(number) {
+      answer.result *= number;
+    },
+    divide: function(number) {
+      answer.result /= number;
+    },
+  };
+
+  return answer;
 }
 
 module.exports = makeCalculator;
