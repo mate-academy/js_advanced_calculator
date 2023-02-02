7cb1c131703168cbccad6cc5e25848412acb2ada
Edgar Kocuba <edgarkocuba@gmail.com>
2023-02-02 22:00:47 +0200

    solution calc advanced

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+    add(a) {
+      calculator.result += a;
+    },
+    subtract(a) {
+      calculator.result -= a;
+    },
+    multiply(a) {
+      calculator.result *= a;
+    },
+    divide(a) {
+      calculator.result /= a;
+    },
+    operate(operation, number) {
+      operation(number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
