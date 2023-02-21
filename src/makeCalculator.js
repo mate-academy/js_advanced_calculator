cb326cd909bec15f96c6a175a237458eaf6d920d
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-02-21 13:27:54 +0200

    Calculator refactor

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,76 +1,43 @@
 'use strict';
 
-/**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
- *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
- *
- * Example:
- * const calculator = makeCalculator();
- *
- * calculator.operate(calculator.add, 21)
- * calculator.result === 21
-
- * calculator.reset()
- * calculator.result === 0
-
- * calculator
- *  .operate(calculator.add, 10)
- *  .reset()
- *  .operate(calculator.subtract, 20)
- *  .operate(calculator.divide, 5)
- *  .operate(calculator.multiply, 7)
- *
- * calculator.result === -28
- *
- *
- * @return {object}
- */
 function makeCalculator() {
-  const obj = {
+  const calculator = {
     result: 0,
 
-    add: (a, b) => {
-      return a + b;
-    },
+    operate(callback, number) {
+      this.result = callback(number);
 
-    subtract: (a, b) => {
-      return a - b;
+      return this;
     },
-
-    multiply: (a, b) => {
-      return a * b;
-    },
-
-    divide: (a, b) => {
-      return a / b;
-    },
-
-    reset: function() {
+    reset() {
       this.result = 0;
 
       return this;
     },
 
-    operate: function(operation, a) {
-      this.result = operation(this.result, a);
+    add(number) {
+      calculator.result += number;
 
-      return this;
+      return calculator.result;
     },
+    subtract(number) {
+      calculator.result -= number;
 
+      return calculator.result;
+    },
+    multiply(number) {
+      calculator.result *= number;
+
+      return calculator.result;
+    },
+    divide(number) {
+      calculator.result /= number;
+
+      return calculator.result;
+    },
   };
 
-  return obj;
+  return calculator;
 }
 
 module.exports = makeCalculator;
