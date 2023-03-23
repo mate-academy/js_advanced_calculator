a908d1f1a57ef6e504f04c6513f24dd46f4d5597
Max Vovk <vovkmax1@gmail.com>
2023-03-23 16:41:52 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,23 +1,5 @@
 'use strict';
 
-/**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
- *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
-
- *
- * @return {object}
- */
 function makeCalculator() {
   const calculator = {
     result: 0,
