2bb569d927142ba42970fdad9817a4bf339e6123
Kacper Łyczba <suaby1725@gmail.com>
2023-04-05 02:07:35 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,42 @@
  */
 function makeCalculator() {
   // write code here
+  /* The instructions speak of 'result property',
+  yet the checklist says NOT to name it 'result'...
+  That's the confusing part in my opinion */
+  const calculator = {
+    result: 0,
+
+    operate(operation, number) {
+      operation(number);
+
+      return calculator;
+    },
+
+    add(number) {
+      calculator.result += number;
+    },
+
+    subtract(number) {
+      calculator.result -= number;
+    },
+
+    multiply(number) {
+      calculator.result *= number;
+    },
+
+    divide(number) {
+      calculator.result /= number;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
