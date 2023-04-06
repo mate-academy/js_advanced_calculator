e5887d31e30d3c8223c9b3d3050d8ed3bb6a9501
Serhii Stepura <stepurasergiy@gmail.com>
2023-04-06 17:25:04 +0300

    solution_calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,38 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const obj = {
+    result: 0,
+    add(num) {
+      this.result += num;
+    },
+
+    subtract(num) {
+      this.result -= num;
+    },
+
+    multiply(num) {
+      this.result *= num;
+    },
+
+    divide(num) {
+      this.result /= num;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate: function(callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
