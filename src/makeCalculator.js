e08a79b7f974eab7ebab8aa6f661ba056ed70aaf
Roman <rdyakovich@gmail.com>
2023-04-24 16:47:45 +0300

    develop

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,19 +1,19 @@
 'use strict';
 
 /**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
+* Ще один калькулятор.Зараз завдання складніше.
+ * Створіть функцію `makecalculator`, яка повертає об'єкт, який
+ * має такі поля:
+ ]
+ * - Властивість `Результат спочатку 0.
  *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
+ * Як працюватиме калькулятор:
+ * - кожен виклик `eperate` приймає зворотний виклик і номер і встановлює
+ * Відповідне значення для властивості `result`.
+ * - Метод `` `скидає значення` result` до 0.
+ ]
+ * `eperate` метод
+ * - Методи `eperate` і` Reset` можна називати в ланцюзі.
  *
  * Example:
  * const calculator = makeCalculator();
@@ -38,6 +38,40 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+
+    add(operand) {
+      this.result += operand;
+    },
+
+    subtract(operand) {
+      this.result -= operand;
+    },
+
+    multiply(operand) {
+      this.result *= operand;
+    },
+
+    divide(operand) {
+      this.result /= operand;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, operand) {
+      callback.call(this, operand);
+
+      return this;
+    },
+
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
