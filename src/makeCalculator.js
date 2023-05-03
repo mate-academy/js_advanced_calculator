e5d2448b8d71057aaa11046a8d500d8c2797b99b
Evhen Ivchenko <ivchenkoevhen@gmail.com>
2023-05-03 15:15:20 +0300

    finished version

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,41 @@
  *
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+
+    add: function(number) {
+      this.result += number;
+    },
+
+    subtract: function(number) {
+      this.result -= number;
+    },
+
+    multiply: function(number) {
+      this.result *= number;
+    },
+
+    divide: function(number) {
+      this.result /= number;
+    },
+
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
