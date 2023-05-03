2d7a12e848be97cce2f3b729a64432bd539b931f
Misha Siv <0Sivcov0@gmail.com>
2023-05-03 20:30:41 +0300

    add calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,47 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(numb) {
+      calculator.result += numb;
+
+      return calculator.result;
+    },
+
+    subtract(numb) {
+      calculator.result -= numb;
+
+      return calculator.result;
+    },
+
+    multiply(numb) {
+      calculator.result *= numb;
+
+      return calculator.result;
+    },
+
+    divide(numb) {
+      calculator.result /= numb;
+
+      return calculator.result;
+    },
+
+    reset: () => {
+      calculator.result = 0;
+
+      return calculator;
+    },
+
+    operate: (callback, number) => {
+      calculator.result = callback(number);
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
