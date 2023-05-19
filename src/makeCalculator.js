c45e6e232b8cade8b46288a563f49ba5542b78ea
Maksym Kostetskyi <mkost@gmail.com>
2023-05-19 15:26:23 +0300

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,43 +1,47 @@
 'use strict';
 
-/**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
- *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
- *
- * Example:
- * const calculator = makeCalculator();
- *
- * calculator.operate(calculator.add, 21)
- * calculator.result === 21
-
- * calculator.reset()
- * calculator.result === 0
-
- * calculator
- *  .operate(calculator.add, 10)
- *  .reset()
- *  .operate(calculator.subtract, 20)
- *  .operate(calculator.divide, 5)
- *  .operate(calculator.multiply, 7)
- *
- * calculator.result === -28
- *
- *
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add: function(number) {
+      calculator.result += number;
+
+      return calculator;
+    },
+
+    subtract: function(number) {
+      calculator.result -= number;
+
+      return calculator;
+    },
+
+    multiply: function(number) {
+      calculator.result *= number;
+
+      return calculator;
+    },
+
+    divide: function(number) {
+      calculator.result /= number;
+
+      return calculator;
+    },
+
+    reset: function() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+
+    operate: function(callback, number) {
+      callback(number);
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
