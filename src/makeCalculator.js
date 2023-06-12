904b92e90d8aba57b219c0b45b250418a7dd6e0d
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-06-12 19:14:33 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const obj = {
+    result: 0,
+
+    add(value) {
+      obj.result += value;
+    },
+
+    subtract(value) {
+      obj.result -= value;
+    },
+
+    multiply(value) {
+      obj.result *= value;
+    },
+
+    divide(value) {
+      obj.result /= value;
+    },
+
+    reset() {
+      obj.result = 0;
+
+      return obj;
+    },
+
+    operate(operation, value) {
+      operation(value);
+
+      return obj;
+    },
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
