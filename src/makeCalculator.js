ac0578fa88a4da969a6c523a1b8cdadbd4fdc28a
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-06-13 16:54:39 +0300

    Added task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate: function(operator, value) {
+      this.result = operator(this.result, value);
+
+      return this;
+    },
+
+    add: function(value1, value2) {
+      return value1 + value2;
+    },
+
+    subtract: function(value1, value2) {
+      return value1 - value2;
+    },
+
+    multiply: function(value1, value2) {
+      return value1 * value2;
+    },
+
+    divide: function(value1, value2) {
+      return value1 / value2;
+    },
+
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
