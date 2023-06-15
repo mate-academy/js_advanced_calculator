3629e6e52a92665dd81b349877cbe6df4e0ac2f2
Marina Olenchenko <marina.olenchenko2204@gmail.com>
2023-06-15 16:17:32 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,45 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(num) {
+      this.result += num;
+
+      return this.result;
+    },
+    subtract(num) {
+      this.result -= num;
+
+      return this.result;
+    },
+    multiply(num) {
+      this.result *= num;
+
+      return this.result;
+    },
+    divide(num) {
+      this.result /= num;
+
+      return this.result;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, value) {
+      callback.call(this, value);
+
+      return this;
+    },
+
+    get finalResult() {
+      return this.result;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
