1bc12fe839419cd335de9edb26c71cab706ba743
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-19 14:47:59 +0300

    Finished task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(n) {
+      this.result += n;
+    },
+
+    subtract(n) {
+      this.result -= n;
+    },
+
+    multiply(n) {
+      this.result *= n;
+    },
+
+    divide(n) {
+      this.result /= n;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, number) {
+      operation.call(this, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
