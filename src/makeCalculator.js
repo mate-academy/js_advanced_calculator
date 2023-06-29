d6660e428cbdf4a17861c747033fb1ae6153ffc6
unknown <fanton81@gmail.com>
2023-06-29 16:17:52 +0300

    mySolution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,34 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+    subtract(x, y) {
+      return x - y;
+    },
+    add(x, y) {
+      return x + y;
+    },
+    divide(x, y) {
+      return x / y;
+    },
+    multiply(x, y) {
+      return x * y;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
