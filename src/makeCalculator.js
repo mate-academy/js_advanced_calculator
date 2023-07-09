a64d27ecb97bd74f701bfb08010e50bd7f7e558f
Ksenia Skrebtsova <121960247+KseniaSkr@users.noreply.github.com>
2023-07-10 00:13:13 +0300

    Calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,26 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(callback, x) {
+      this.result = callback(this.result, x);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+  };
 }
 
 module.exports = makeCalculator;
