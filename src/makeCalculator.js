3156b8abaf0ee27b05cc239c8b5f835d1eeccc91
Anton Bilous <toxanteloxran@gmail.com>
2023-07-17 19:59:19 +0300

    Test error

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,43 +37,44 @@
  * @return {object}
  */
 function makeCalculator() {
-  let result = 0;
-
   const calculator = {
+    total: 0,
+
     operate(callback, value) {
-      callback(value);
+      this.total = callback(value);
 
       return calculator;
     },
     add(value) {
-      result += value;
+      this.total += value;
 
       return calculator;
     },
     reset() {
-      result = 0;
+      this.total = 0;
 
       return calculator;
     },
     subtract(value) {
-      result -= value;
+      this.total -= value;
 
       return calculator;
     },
     divide(value) {
-      if (value !== 0) {
-        result /= value;
+      if (value === 0) {
+        return 'Error - division by 0';
       }
+      this.total /= value;
 
       return calculator;
     },
     multiply(value) {
-      result *= value;
+      this.total *= value;
 
       return calculator;
     },
     get result() {
-      return result;
+      return this.total;
     },
   };
 
