a907b1fe739fa87f6de83f07cc8b6195a7b801c2
Platon Timonin <platonidze1984@gmail.com>
2023-07-23 17:39:54 +0300

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    operate: function(operation, value) {
+      return operation.call(this, value);
+    },
+    add: function(x) {
+      this.result += x;
+
+      return this;
+    },
+    subtract: function(x) {
+      this.result -= x;
+
+      return this;
+    },
+    multiply: function(x) {
+      this.result *= x;
+
+      return this;
+    },
+    divide: function(x) {
+      if (x !== 0) {
+        this.result /= x;
+      }
+
+      return this;
+    },
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
