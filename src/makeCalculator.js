afe7561ed1a634012d997b5d562ffe9c6a02bfd7
Serhii Dranuta <sirojka8@gmail.com>
2023-07-23 12:37:27 +0300

    Add makeCalculator solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    add(num) {
+      result += num;
+
+      return this;
+    },
+    subtract(num) {
+      result -= num;
+
+      return this;
+    },
+    multiply(num) {
+      result *= num;
+
+      return this;
+    },
+    divide(num) {
+      result /= num;
+
+      return this;
+    },
+    reset() {
+      result = 0;
+
+      return this;
+    },
+    operate(method, num) {
+      method(num);
+
+      return this;
+    },
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
