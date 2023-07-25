1820cb4db43e282d6f7882d99cd9b4d43d123d18
ZakZh <38009936+ZakZh@users.noreply.github.com>
2023-07-25 22:18:37 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,57 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    checkValue(val) {
+      if (val && typeof (val) === 'number') {
+        return this;
+      } else {
+        throw new Error('Invalid value!');
+      }
+    },
+
+    add(val) {
+      this.result += val;
+
+      return this;
+    },
+
+    subtract(val) {
+      this.result -= val;
+
+      return this;
+    },
+
+    multiply(val) {
+      this.result *= val;
+
+      return this;
+    },
+
+    divide(val) {
+      this.result /= val;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, val) {
+      if (typeof callback === 'function' && this.checkValue(val)) {
+        const bindCallback = callback.bind(this);
+
+        return bindCallback(val);
+      }
+
+      throw new Error(`${callback} is not an operator in this calc`);
+    },
+  };
 }
 
 module.exports = makeCalculator;
