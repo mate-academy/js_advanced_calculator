b5f4b4b1c0a8560fbff1ae1c816977821a1d464e
Andrzej Pec <pimatstudio@gmail.com>
2023-07-27 00:03:49 +0200

    Crazy solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
@@ -36,6 +37,12 @@
  *
  * @return {object}
  */
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 function makeCalculator() {
   const calculator = {
     result: 0,
@@ -43,8 +50,11 @@ function makeCalculator() {
     subtract: (a, b) => a - b,
     multiply: (a, b) => a * b,
     divide: (a, b) => a / b,
-    operate(callback, value) {
-      this.result = callback(this.result, value);
+    operate(operation, value) {
+      if (typeof value !== 'number') {
+        throw new Error('value must be a number');
+      }
+      this.result = operation(this.result, value);
 
       return this;
     },
@@ -53,9 +63,42 @@ function makeCalculator() {
 
       return this;
     },
+    prompt() {
+      rl.question(`Choose the operation (add, subtract, multiply, divide, quit): `,
+        (operation) => {
+          if (operation === 'quit') {
+            console.log(`Final result is: ${this.result}`);
+            rl.close();
+          } else {
+            rl.question('Choose the value: ', (value) => {
+              switch (operation) {
+                case 'add':
+                  this.operate(this.add, Number(value));
+                  break;
+                case 'subtract':
+                  this.operate(this.subtract, Number(value));
+                  break;
+                case 'multiply':
+                  this.operate(this.multiply, Number(value));
+                  break;
+                case 'divide':
+                  this.operate(this.divide, Number(value));
+                  break;
+                default:
+                  console.log('Invalid operation');
+              }
+              console.log(`The result is: ${this.result}`);
+              this.prompt();
+            });
+          }
+        });
+    },
   };
 
   return calculator;
 }
 
+const calculate = makeCalculator();
+
+calculate.prompt();
 module.exports = makeCalculator;
