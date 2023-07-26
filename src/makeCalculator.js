606d545347f8e4f3346861ae25702137a978fd3f
Andrzej Pec <pimatstudio@gmail.com>
2023-07-26 23:39:09 +0200

    Initial solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,25 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
