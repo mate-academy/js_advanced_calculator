aa6984728e634596b32479741043c4a86e347d57
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-07-27 15:17:25 +0200

    made calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,51 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const calc = {
+    add: function(number) {
+      result += number;
+
+      return this;
+    },
+
+    subtract: function(number) {
+      result -= number;
+
+      return this;
+    },
+
+    multiply: function(number) {
+      result *= number;
+
+      return this;
+    },
+
+    divide: function(number) {
+      result /= number;
+
+      return this;
+    },
+
+    reset: function(number) {
+      result = 0;
+
+      return this;
+    },
+
+    operate: function(callback, number) {
+      callback(number);
+
+      return this;
+    },
+
+    get result() {
+      return result;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
