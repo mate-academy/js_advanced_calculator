c1d25d0101b529515b61044665c2ce91c3bc2014
Anna Shevtsova <aniashevtsova@gmail.com>
2023-07-27 11:43:12 +0200

    all tests pass

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,38 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate(callback, value) {
+      callback.call(this, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(value) {
+      this.result += value;
+    },
+
+    subtract(value) {
+      this.result -= value;
+    },
+
+    multiply(value) {
+      this.result *= value;
+    },
+
+    divide(value) {
+      this.result /= value;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
