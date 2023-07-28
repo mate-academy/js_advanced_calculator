e932aad3d2a90f1d7a98e7fb40edc1bf6bd90d7b
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-07-28 05:38:45 +0300

    add task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add: function(number) {
+      this.result += number;
+    },
+
+    subtract: function(number) {
+      this.result -= number;
+    },
+
+    multiply: function(number) {
+      this.result *= number;
+    },
+
+    divide: function(number) {
+      this.result /= number;
+    },
+
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate: function(calculator, number) {
+      calculator.call(this, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
