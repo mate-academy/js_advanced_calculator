aa3bed55d2087547792785c1ce23627780f6c985
Kacper <leman_kacper@o2.pl>
2023-08-01 12:28:39 +0200

    Calculator done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,31 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    operate: function(operation, value) {
+      this.result = operation(this.result, value);
+
+      return this;
+    },
+    add: function(a, b) {
+      return a + b;
+    },
+    subtract: function(a, b) {
+      return a - b;
+    },
+    multiply: function(a, b) {
+      return a * b;
+    },
+    divide: function(a, b) {
+      return a / b;
+    },
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
