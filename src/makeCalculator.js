deed5f9744a04d595829e7f59c9bd0d80ca4e6a1
Maksym Borysenko <bormaxv@gmail.com>
2023-08-04 15:17:11 +0300

    solved task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,44 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    operate: function(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(num) {
+      this.result += num;
+
+      return this;
+    },
+
+    subtract(num) {
+      this.result -= num;
+
+      return this;
+    },
+
+    multiply(num) {
+      this.result *= num;
+
+      return this;
+    },
+
+    divide(num) {
+      this.result /= num;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
