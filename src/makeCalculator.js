6e584ad131aa064a8329b968b09c2b2ad76ab99a
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-08-06 16:19:35 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -42,32 +42,22 @@ function makeCalculator() {
 
     add(value) {
       this.result += value;
-
-      return this;
     },
 
     subtract(value) {
       this.result -= value;
-
-      return this;
     },
 
     multiply(value) {
       this.result *= value;
-
-      return this;
     },
 
     divide(value) {
       this.result /= value;
-
-      return this;
     },
 
     reset() {
       this.result = 0;
-
-      return this;
     },
 
     operate(callback, value) {
