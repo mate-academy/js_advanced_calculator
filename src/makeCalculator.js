94bb9f6969803940800ad17ef9b59c6fe2415968
Nazar Kyzenko <kyzenko1997@gmail.com>
2023-08-07 21:17:17 +0300

    TaskDoneAttempOne

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,37 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    operate(operation, number) {
+      this.result = operation(this.result, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      return a / b;
+    },
+  };
 }
 
 module.exports = makeCalculator;
