9e5c949b84ec00d0b13feecfa00a0c88e2d39ee7
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-08-08 12:32:33 +0300

    fix task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -41,23 +41,15 @@ function makeCalculator() {
     result: 0,
     add(value) {
       this.result += value;
-
-      return this;
     },
     subtract(value) {
       this.result -= value;
-
-      return this;
     },
     divide(value) {
       this.result /= value;
-
-      return this;
     },
     multiply(value) {
       this.result *= value;
-
-      return this;
     },
     reset() {
       this.result = 0;
