28a8469c89e997508f13d05dd9ba49360e719843
Ekaterina Bessonnaya <iekatierinovna9090@gmail.com>
2023-08-09 12:16:29 +0300

    calculator was created

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,36 @@
  *
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
