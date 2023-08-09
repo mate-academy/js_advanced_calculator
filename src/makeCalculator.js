c50b0468eee9af37e4d8422b5e81693250fc9212
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-09 09:23:56 +0300

    initial commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -41,33 +41,21 @@ function makeCalculator() {
     result: 0,
     add(number) {
       this.result += number;
-
-      return this;
     },
     subtract(number) {
       this.result -= number;
-
-      return this;
     },
     multiply(number) {
       this.result *= number;
-
-      return this;
     },
     divide(number) {
       this.result /= number;
-
-      return this;
     },
     reset() {
       this.result = 0;
-
-      return this;
     },
     operate(action, number) {
       action.call(this, number);
-
-      return this;
     },
   };
 }
