7bd5451eaede8515849c44c0bc1cb0325ae7c57c
Oleg Klivanets <oleg.klivanets@gmail.com>
2023-08-14 21:26:24 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,38 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    add(x) {
+      calculator.result += x;
+    },
+
+    subtract(x) {
+      calculator.result -= x;
+    },
+
+    divide(x) {
+      calculator.result /= x;
+    },
+
+    multiply(x) {
+      calculator.result *= x;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+
+    operate(callback, x) {
+      callback(x);
+
+      return this;
+    },
+    result: 0,
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
