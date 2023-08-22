cce060b5d650da3b8b29fee4907b414ac3913c31
Artem Trofymovych <navrotskymetra@gmail.com>
2023-08-22 10:14:16 +0200

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add: function(num) {
+      this.result += num;
+
+      return this;
+    },
+    subtract: function(num) {
+      this.result -= num;
+
+      return this;
+    },
+    multiply: function(num) {
+      this.result *= num;
+
+      return this;
+    },
+    divide: function(num) {
+      this.result /= num;
+
+      return this;
+    },
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function(callback, num) {
+      return callback.call(this, num);
+    },
+  };
 }
 
 module.exports = makeCalculator;
