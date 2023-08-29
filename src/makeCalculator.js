4dd4a12bc8b08e9af2d76a1d2d76113dc6f87589
Daniil <danil.beshlebnov@gmail.com>
2023-08-29 17:01:04 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,45 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const calculator = {
+    add: (num) => {
+      result += num;
+
+      return calculator;
+    },
+    subtract: (num) => {
+      result -= num;
+
+      return calculator;
+    },
+    multiply: (num) => {
+      result *= num;
+
+      return calculator;
+    },
+    divide: (num) => {
+      result /= num;
+
+      return calculator;
+    },
+    reset: () => {
+      result = 0;
+
+      return calculator;
+    },
+    operate: (callback, num) => {
+      callback(num);
+
+      return calculator;
+    },
+    get result() {
+      return result;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
