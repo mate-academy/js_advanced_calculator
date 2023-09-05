be086171c04fdaf4699c6c8458d019bd085e10cb
Denys Vasylenkoe <kapodenim@gmail.com>
2023-09-05 06:42:55 +0300

    v1

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,36 @@
  */
 function makeCalculator() {
   // write code here
-}
+  return {
+    result: 0,
 
+    add(number) {
+      this.result += number;
+    },
+
+    subtract(number) {
+      this.result -= number;
+    },
+
+    multiply(number) {
+      this.result *= number;
+    },
+
+    divide(number) {
+      this.result /= number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate: function(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+  };
+}
 module.exports = makeCalculator;
