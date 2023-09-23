d129a8e18b8226cf0a16bbdff57759e71d0ccf16
Oleksandr Chumakov <Chumakvs2001@gmail.com>
2023-09-23 22:19:41 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,46 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    add: function(value) {
+      result += value;
+
+      return this;
+    },
+    subtract: function(value) {
+      result -= value;
+
+      return this;
+    },
+    multiply: function(value) {
+      result *= value;
+
+      return this;
+    },
+    divide: function(value) {
+      if (value === 0) {
+        throw new Error('Division by zero is not allowed.');
+      }
+      result /= value;
+
+      return this;
+    },
+    reset: function() {
+      result = 0;
+
+      return this;
+    },
+    operate: function(callback, value) {
+      callback(value);
+
+      return this;
+    },
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
