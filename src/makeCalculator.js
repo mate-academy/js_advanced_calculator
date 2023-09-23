a46a72234cd7b6e362a908542ca63f03a5fe91f5
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2023-09-23 15:17:29 +0300

    Calculator objects 1st try

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,46 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, num) {
+      callback(this, num);
+
+      return this;
+    },
+    add: (obj, num) => {
+      obj.result += num;
+
+      return obj;
+    },
+
+    subtract: (obj, num) => {
+      obj.result -= num;
+
+      return obj;
+    },
+
+    multiply: (obj, num) => {
+      obj.result *= num;
+
+      return obj;
+    },
+
+    divide: (obj, num) => {
+      obj.result = obj.result / num;
+
+      return obj;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
