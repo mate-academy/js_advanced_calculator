2faf86c7b3539b9f16ec3facf81cec99ec90cfce
Kseniia Chepur <kseniia.chepur@gmail.com>
2023-10-06 14:48:10 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(x, y) {
+      return x + y;
+    },
+
+    subtract(x, y) {
+      return x - y;
+    },
+
+    multiply(x, y) {
+      return x * y;
+    },
+
+    divide(x, y) {
+      return x / y;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, x) {
+      this.result = operation(this.result, x);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
