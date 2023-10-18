d140810f5e14fb2ace9f1749df45662a59112b96
Kateryna <kate042798@gmail.com>
2023-10-17 21:32:20 -0600

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add: (number) => {
+      calculator.result += number;
+
+      return calculator;
+    },
+    subtract: (number) => {
+      calculator.result -= number;
+
+      return calculator;
+    },
+    multiply: (number) => {
+      calculator.result *= number;
+
+      return calculator;
+    },
+    divide: (number) => {
+      calculator.result /= number;
+
+      return calculator;
+    },
+    operate: (callback, number) => {
+      calculator.result = callback(calculator.result, number);
+
+      return calculator;
+    },
+    reset: () => {
+      calculator.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
