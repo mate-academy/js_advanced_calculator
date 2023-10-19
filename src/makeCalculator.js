0e7002ec14f910a51a9aac69ff12b58d30a48717
Denys Stronin <den.stronin@gmail.com>
2023-10-19 12:32:02 +0300

    Advanced calculator - First commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,39 @@
  */
 function makeCalculator() {
   // write code here
+  const resultObj = {
+    result: 0,
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      return a / b;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(method, number) {
+      this.result = method(this.result, number);
+
+      return this;
+    },
+  };
+
+  return resultObj;
 }
 
 module.exports = makeCalculator;
