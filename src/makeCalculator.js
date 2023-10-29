9dc49d7a33187e824f553d73305568f58a75bfaa
Eater228 <543145yura@gmail.com>
2023-10-29 10:25:41 +0200

    Add Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,50 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const add = (number) => {
+    result += number;
+  };
+
+  const subtract = (number) => {
+    result -= number;
+  };
+
+  const multiply = (number) => {
+    result *= number;
+  };
+
+  const divide = (number) => {
+    if (number === 0) {
+      throw new Error('Division by zero is not allowed');
+    }
+    result /= number;
+  };
+
+  function reset() {
+    result = 0;
+
+    return this;
+  }
+
+  function operate(callback, number) {
+    callback(number);
+
+    return this;
+  }
+
+  return {
+    add,
+    subtract,
+    multiply,
+    divide,
+    reset,
+    operate,
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
