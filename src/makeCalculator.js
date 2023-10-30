811bb607696acb3e7742c222daca188fa0d821c3
Maksym Svoboda <maksym.svoboda.kn.2021@lpnu.ua>
2023-10-30 16:04:54 +0200

    added spaces and changed invocation method

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,31 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  return {
+  const calculator = {
     result: 0,
-    add(instance, num) {
-      instance.result += num;
+
+    add(num) {
+      this.result += num;
     },
-    subtract(instance, num) {
-      instance.result -= num;
+
+    subtract(num) {
+      this.result -= num;
     },
-    multiply(instance, num) {
-      instance.result *= num;
+
+    multiply(num) {
+      this.result *= num;
     },
-    divide(instance, num) {
-      instance.result /= num;
+
+    divide(num) {
+      this.result /= num;
     },
+
     reset() {
       this.result = 0;
 
       return this;
     },
+
     operate(calcAction, num) {
-      calcAction(this, num);
+      calcAction.call(this, num);
 
       return this;
     },
   };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
