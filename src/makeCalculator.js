c1df246e911b008141a5df6eb6cd2154962feed2
Olesia Semen <semenolesa01@gmail.com>
2023-10-30 23:11:38 +0200

    Testing

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,41 @@
  *
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
-}
+  function makeCalculator() {
+    const calculator = {
+      result: 0,
+  
+      add: function (value) {
+        this.result += value;
+        return this;
+      },
+  
+      subtract: function (value) {
+        this.result -= value;
+        return this;
+      },
+  
+      multiply: function (value) {
+        this.result *= value;
+        return this;
+      },
+  
+      divide: function (value) {
+        this.result /= value;
+        return this;
+      },
+  
+      reset: function () {
+        this.result = 0;
+        return this;
+      },
+  
+      operate: function (operation, value) {
+        return operation.call(this, value);
+      },
+    };
+  
+    return calculator;
+  }
 
 module.exports = makeCalculator;
