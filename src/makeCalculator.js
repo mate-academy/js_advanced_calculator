11b38a3702d37cc72ec702af0243791f6b6c97e6
Vladislav Balaban <vlad.balaban2020@gmail.com>
2023-10-31 21:02:33 +0200

    Added calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -42,26 +42,31 @@ function makeCalculator() {
 
     add: function (num) {
       this.result += num;
+
       return this;
     },
 
     subtract: function (num) {
       this.result -= num;
+
       return this;
     },
 
     multiply: function (num) {
       this.result *= num;
+
       return this;
     },
 
     divide: function (num) {
       this.result /= num;
+
       return this;
     },
 
     reset: function () {
       this.result = 0;
+
       return this;
     },
 
