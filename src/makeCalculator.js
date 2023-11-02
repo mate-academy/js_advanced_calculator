bb9908d2ad17e3f8bb1bd85591eea36c7d878021
Olesia Semen <semenolesa01@gmail.com>
2023-11-02 17:50:09 +0200

    Testing

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,42 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add: function(value) {
+      calculator.result += value;
+
+      return calculator;
+    },
+    subtract: function(value) {
+      calculator.result -= value;
+
+      return calculator;
+    },
+    multiply: function(value) {
+      calculator.result *= value;
+
+      return calculator;
+    },
+    divide: function(value) {
+      if (value === 0) {
+        throw new Error('Division by zero is not allowed');
+      }
+      calculator.result /= value;
+
+      return calculator;
+    },
+    reset: function() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+    operate: function(callback, value) {
+      return callback(value);
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
