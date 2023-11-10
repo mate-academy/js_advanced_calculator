540e89823ff35f6f872615888e68d926623cdd9b
Oleksandra Koshyk <sakoshik1984@gmail.com>
2023-11-10 19:22:04 +0100

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,42 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(result, number) {
+      return result + number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    subtract(result, number) {
+      return result - number;
+    },
+
+    multiply(result, number) {
+      return result * number;
+    },
+
+    divide(result, number) {
+      if (number !== 0) {
+        return result / number;
+      }
+
+      return 0;
+    },
+
+    operate(action, number) {
+      this.result = action(this.result, number);
+
+      return this;
+    },
+
+  };
 }
 
 module.exports = makeCalculator;
