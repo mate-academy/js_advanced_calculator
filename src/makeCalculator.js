16b12da2096ec017bc7aae80cea3ef72e759f530
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2023-11-17 13:05:38 +0200

    calculator advanced solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,47 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add: function(value) {
+      this.result += value;
+
+      return this;
+    },
+
+    subtract: function(value) {
+      this.result -= value;
+
+      return this;
+    },
+
+    multiply: function(value) {
+      this.result *= value;
+
+      return this;
+    },
+
+    divide: function(value) {
+      if (value !== 0) {
+        this.result /= value;
+      } else {
+        return Infinity;
+      }
+
+      return this;
+    },
+
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate: function(callback, value) {
+      return callback.call(this, value);
+    },
+  };
 }
 
 module.exports = makeCalculator;
