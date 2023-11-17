3e3aa17bc5ee604e955ffe588be5109878cd4480
Wojciech Sęk <wojtasskate@wp.pl>
2023-11-17 11:59:36 +0100

    version 2

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,67 +1,32 @@
 'use strict';
 
-/**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
- *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
- *
- * Example:
- * const calculator = makeCalculator();
- *
- * calculator.operate(calculator.add, 21)
- * calculator.result === 21
-
- * calculator.reset()
- * calculator.result === 0
-
- * calculator
- *  .operate(calculator.add, 10)
- *  .reset()
- *  .operate(calculator.subtract, 20)
- *  .operate(calculator.divide, 5)
- *  .operate(calculator.multiply, 7)
- *
- * calculator.result === -28
- *
- *
- * @return {object}
- */
 function makeCalculator() {
   return {
     result: 0,
     reset: function() {
       this.result = 0;
+
+      return this;
     },
     operate: function(operate, number) {
-      let numberFrom = this.result;
-      operate(number,numberFrom);
+      this.result = operate(this.result, number);
 
       return this;
     },
     add: function(a, b) {
       return a + b;
     },
-    subtract: function(b) {
-      this.result -= b;
+    subtract: function(a, b) {
+      return a - b;
     },
-    multiply: function(c) {
-      this.result *= c;
+    multiply: function(a, b) {
+      return a * b;
     },
-    divide: function(d) {
-      if (d !== 0) {
-        this.result /= d;
+    divide: function(a, b) {
+      if (b !== 0) {
+        return a / b;
       } else {
-        return "can't divide by zero";
+        throw new Error("Don't divide by zero");
       }
     },
 
