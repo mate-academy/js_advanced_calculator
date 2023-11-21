7465e15568f9d1b45ca7a0ae4e0ee6fb982f8969
Denis Reznik <reznik.d.o12@gmail.com>
2023-11-21 20:18:09 +0200

    Fix solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -56,6 +56,10 @@ function makeCalculator() {
       return operator - number;
     },
     divide(number, operator) {
+      if (number === 0) {
+        return Error(`You can't divide on 0`);
+      }
+
       return operator / number;
     },
     multiply(number, operator) {
