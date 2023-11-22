cab679d0e22f3aee66d46721f7450fc5faa7d762
dmytrosantarskyi <dsantarskyi@gmail.com>
2023-11-22 11:06:39 +0100

    task for approve

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,32 @@
  *
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
+ function makeCalculator() {
+  return {
+    result: 0,
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(way, num) {
+      this.result = way(this.result, num);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
