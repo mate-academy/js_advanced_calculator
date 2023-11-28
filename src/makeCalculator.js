c0e8bd2b87ce358dd69785287d4280a90dbaa359
Iryna Lysko <goiralysko@gmail.com>
2023-11-28 21:38:00 +0200

    add advanced calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    add(number) {
+      this.result += number;
+    },
+
+    subtract(number) {
+      this.result -= number;
+    },
+
+    multiply(number) {
+      this.result *= number;
+    },
+
+    divide(number) {
+      if (number !== 0) {
+        this.result /= number;
+      }
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+
+    result: 0,
+  };
 }
 
 module.exports = makeCalculator;
