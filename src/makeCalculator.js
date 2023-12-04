ef4c300b4aed3bb2428f7b48d8877bd934b266ea
Dmytro Kupriiev <d.kupriev@gmail.com>
2023-12-04 16:13:33 +0200

    Division by zero checked

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -74,7 +74,11 @@ function makeCalculator() {
     }
 
     divide(num1, num2) {
-      return num1 / num2;
+      if (num2 !== 0) {
+        return num1 / num2;
+      }
+
+      throw new Error('Division by zero');
     }
   }
 
