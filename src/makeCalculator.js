bd39c0b75aac58f93a553ec95200a9bbff9e1129
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2023-12-11 19:29:07 +0200

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,47 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+
+      return this;
+    },
+
+    subtract(value) {
+      this.result -= value;
+
+      return this;
+    },
+
+    multiply(value) {
+      this.result *= value;
+
+      return this;
+    },
+
+    divide(value) {
+      if (value !== 0) {
+        this.result /= value;
+      }
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, value) {
+      operation.call(this, value);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
