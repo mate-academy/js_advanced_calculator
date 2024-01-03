f7b23af126e8abdd50f2749e15a3acb5182b13d0
Vitalii Labunskyi <vitold85@gmail.com>
2024-01-03 10:51:01 +0200

    calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,23 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
