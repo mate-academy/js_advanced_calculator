ea4b432c7cde282f4d6d102e12147e98520d3e05
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-01-07 14:54:04 +0200

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,41 @@
  *
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(currentResult, number) {
+      return currentResult + number;
+    },
+
+    subtract(currentResult, number) {
+      return currentResult - number;
+    },
+
+    multiply(currentResult, number) {
+      return currentResult * number;
+    },
+
+    divide(currentResult, number) {
+      return currentResult / number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
