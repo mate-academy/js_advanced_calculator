e9eda6bea85043c4bdba901b1f3d68d62ca1b785
“Julia <j.shkorupatska@gmail.com>
2024-01-13 01:13:37 +0200

    added error divided by zero

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -44,7 +44,10 @@ function makeCalculator() {
 
     multiply: (num1, num2) => num1 * num2,
 
-    divide: (num1, num2) => num1 / num2,
+    divide: (num1, num2) =>
+      num2 === 0
+        ? new Error('Cannot be divided by zero')
+        : num1 / num2,
 
     operate(operation, argument) {
       this.result = operation(this.result, argument);
