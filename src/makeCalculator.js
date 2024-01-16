cdc5618e15e89bb3c4916cb6fe359f3cd8f389a7
Artur Yaroshenko <free.shok2@gmail.com>
2024-01-16 14:50:41 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,21 +1,22 @@
 'use strict';
 
 /**
- * Another calculator. Now the task is more difficult.
- * Create a `makeCalculator` function that returns an object that
- * has the following fields:
- *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `result` property is initially 0.
+ * Ще один калькулятор. Тепер завдання складніше.
+ * Створіть функцію `makeCalculator`, яка повертає об'єкт, що
+ * має наступні поля:
+ * - Методи: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
+ * - Властивість `result` початково дорівнює 0.
  *
- * How the calculator will work:
- * - Each `operate` call takes a callback and a number and sets the
- *   appropriate value to the `result` property.
- * - The `reset` method resets `result` value to 0.
- * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
- *   `operate` method
- * - The `operate` and `reset` methods can be called in a chain.
+ * Як буде працювати калькулятор:
+ * - Кожен виклик `operate` приймає зворотний виклик та число і встановлює
+ * відповідне значення властивості `result`.
+ * - Метод `reset` скидає значення `result` до 0.
+ * - Функції `add`, `subtract`, `multiply`, `divide`
+ * передаються як зворотні виклики до
+ * методу `operate`.
+ * - Методи `operate` та `reset` можна викликати ланцюжком.
  *
- * Example:
+ * Приклад:
  * const calculator = makeCalculator();
  *
  * calculator.operate(calculator.add, 21)
@@ -37,7 +38,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+
+    add(x, y) {
+      return x + y;
+    },
+
+    subtract(x, y) {
+      return x - y;
+    },
+
+    multiply(x, y) {
+      return x * y;
+    },
+
+    divide(x, y) {
+      return x / y;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
