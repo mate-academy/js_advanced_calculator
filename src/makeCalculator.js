4849a385c6c58c86ebedf0a9c0085f4cb76a4484
beliyandr <beliyandr@gmail.com>
2024-01-19 13:23:05 +0200

    fixed / deleted all comments / fix function divide

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,35 +1,24 @@
 'use strict';
 
-/*
- * @return {object}
- */
-
 function makeCalculator() {
   const operates = {
 
-    // змінна в якій буде зберігатись результат
     resultMathOperations: 0,
 
-    // геттер який повертає результат , тобто нашу змінну з результатом
     get result() {
       return this.resultMathOperations;
     },
 
-    // сеттер - який буде змінювати нашу результуючу змінну
     set changeResult(value) {
       this.resultMathOperations = value;
     },
 
-    // дана функція приймає (operation) і число (value) - для маніпуляцій з ним
     operate(operation, value) {
       this.changeResult = operation(this.resultMathOperations, value);
 
       return this;
     },
 
-    // Функціі ... , які прииймають початкове(або попереднє) значення
-    // (а саме результат) та число яке передається (value),
-    // та вертає результат 'додавання та інші'
     add(prevValue, value) {
       const result = prevValue + value;
 
@@ -49,12 +38,15 @@ function makeCalculator() {
     },
 
     divide(prevValue, value) {
+      if (value === 0) {
+        return 0;
+      }
+
       const result = prevValue / value;
 
       return result;
     },
 
-    // метод reset - встановлює нашій змінній значення 0
     reset() {
       this.changeResult = 0;
 
