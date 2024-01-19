514d7f760022011df576556691618517c3a3c769
Alena Sokol <alenashpakova27021995@gmail.com>
2024-01-19 15:08:53 +0200

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,39 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, value) {
+      this.result = callback(value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(a) {
+      return calculator.result + a;
+    },
+
+    subtract(a) {
+      return calculator.result - a;
+    },
+
+    multiply(a) {
+      return calculator.result * a;
+    },
+
+    divide(a) {
+      return calculator.result / a;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
