e858eea2f3d63fc8a1228761cf6c6c24ead5a9fb
Anna <annashcheglova.06.08@gmail.com>
2024-01-19 11:54:32 +0200

    calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -39,20 +39,25 @@
 function makeCalculator() {
   const calculator = {
     result: 0,
-    add: (number) => calculator.result + number,
-    subtract: (number) => calculator.result - number,
-    multiply: (number) => calculator.result * number,
-    divide: (number) => calculator.result / number,
-    operate: (callback, number) => {
-      calculator.result = callback(number);
+    add: (result, number) => result + number,
+    subtract: (result, number) => result - number,
+    multiply: (result, number) => result * number,
+    divide: (result, number) => {
+      if (number === 0) {
+        return 'cant divide on 0';
+      }
 
-      return calculator;
+      return result / number;
     },
+    operate(callback, number) {
+      this.result = callback(this.result, number);
 
-    reset: () => {
-      calculator.result = 0;
+      return this;
+    },
+    reset() {
+      this.result = 0;
 
-      return calculator;
+      return this;
     },
   };
 
