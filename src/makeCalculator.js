c17a2dffdd8031027906934498dbb4e6e9db14a4
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-01-25 19:49:57 +0200

    add tusk solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    operate: function(operation, number) {
+      operation.bind(this)(number);
+
+      return this;
+    },
+    reset: function() {
+      this.result = 0;
+
+      return this;
+    },
+    add: function(number) {
+      this.result += number;
+
+      return this;
+    },
+    subtract: function(number) {
+      this.result -= number;
+
+      return this;
+    },
+    multiply: function(number) {
+      this.result *= number;
+
+      return this;
+    },
+    divide: function(number) {
+      this.result = this.result / number;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
