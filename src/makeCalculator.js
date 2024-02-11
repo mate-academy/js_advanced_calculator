06736dc6ee1b1fbb1b8db661a6f29e1f573aa21f
Max Petrashishin <rikogjdefolt@gmail.com>
2024-02-11 22:07:10 +0200

    create calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,23 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add: (res, numb) => res + numb,
+    subtract: (res, numb) => res - numb,
+    multiply: (res, numb) => res * numb,
+    divide: (res, numb) => res / numb,
+    operate(cb, num) {
+      this.result = cb(this.result, num);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
