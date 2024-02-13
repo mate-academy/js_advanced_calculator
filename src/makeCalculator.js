a4fdfe0810979f9c5d81dcb2d586431ebb054815
mfcastilho <fredericocastilho@hotmail.com>
2024-02-13 04:20:58 -0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
@@ -36,8 +37,54 @@
  *
  * @return {object}
  */
+
+// let result = 0;
+
 function makeCalculator() {
-  // write code here
+  const obj = {
+    result: 0,
+
+    add(value) {
+      let result = this.result;
+
+      result += value;
+
+      return result;
+    },
+    subtract(value) {
+      let result = this.result;
+
+      result -= value;
+
+      return result;
+    },
+    multiply(value) {
+      let result = this.result;
+
+      result *= value;
+
+      return result;
+    },
+    divide(value) {
+      let result = this.result;
+
+      result /= value;
+
+      return result;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, value) {
+      this.result = callback.bind(this)(value);
+
+      return this;
+    },
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
