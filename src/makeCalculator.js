55a96554d1cca8304a874cae124de2cb7e69bc51
HolyRaveOS <kitlaruk1995@gmail.com>
2024-02-14 17:32:59 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,47 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  function add(value) {
+    result += value;
+  }
+
+  function subtract(value) {
+    result -= value;
+  }
+
+  function multiply(value) {
+    result *= value;
+  }
+
+  function divide(value) {
+    result /= value;
+  }
+
+  function reset() {
+    result = 0;
+
+    return this;
+  }
+
+  function operate(callback, value) {
+    callback(value);
+
+    return this;
+  }
+
+  return {
+    add,
+    subtract,
+    multiply,
+    divide,
+    reset,
+    operate,
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
