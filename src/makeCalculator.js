d8cae2318455531dedfbec465f38b0e3c2c48c2d
Taisiia Solonichenko <ts.max.98@gmail.com>
2024-02-17 22:25:38 +0200

    create calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,39 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operator, operand) {
+      this.result = operator(operand, this.result);
+
+      return this;
+    },
+
+    add(value, score) {
+      return score + value;
+    },
+
+    subtract(value, score) {
+      return score - value;
+    },
+
+    multiply(value, score) {
+      return score * value;
+    },
+
+    divide(value, score) {
+      return score / value;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
