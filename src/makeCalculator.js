d300126f9e366479bea2a4f5c5ff903c0cd6034c
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-03-06 23:37:05 +0200

    Make task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -37,7 +37,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate: function (callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+    add: function (num) {
+      this.result += num;
+
+      return this;
+    },
+    subtract: function (num) {
+      this.result -= num;
+
+      return this;
+    },
+    multiply: function (num) {
+      this.result *= num;
+
+      return this;
+    },
+    divide: function (num) {
+      this.result /= num;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
