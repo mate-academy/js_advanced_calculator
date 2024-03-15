63f29f2657cc8d46acb7b3f0be73ca571894a3b2
svyatoslav <svyatoslavik9@gmail.com>
2024-03-15 13:47:57 +0100

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -38,6 +38,42 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+    operate (callback, value) {
+      callback.call(this, value);
+      return this;
+    },
+    add (value)  {
+      this.result += value;
+    },
+    subtract  (value) {
+      this.result -= value;
+    },
+    multiply (value) {
+      this.result *= value;
+    },
+    divide (value) {
+      if (value === 0) {
+        throw new Error("Not possible for this action")
+      }
+      this.result /= value;
+    },
+    reset () {
+      this.result = 0;
+      return this;
+    }
+  }
 }
 
-module.exports = makeCalculator;
+const calculator = makeCalculator();
+
+calculator
+.operate(calculator.add, 20)
+.reset()
+.operate(calculator.add, 20)
+.operate(calculator.divide, 625)
+
+console.log(calculator.result);
+
+
