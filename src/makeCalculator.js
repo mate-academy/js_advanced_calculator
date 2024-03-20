b3504d3414818f313c51dc050f2aa78d48de7024
petrushinsky-kirill <petrushinsky@gmail.com>
2024-03-20 16:01:34 +0200

    js_advanced_calculator solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,44 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let calculator = {};
+
+  calculator = {
+    result: 0,
+
+    add(num) {
+      this.result += num;
+
+      return this;
+    },
+    subtract(num) {
+      this.result -= num;
+
+      return this;
+    },
+    multiply(num) {
+      this.result *= num;
+
+      return this;
+    },
+    divide(num) {
+      this.result /= num;
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
