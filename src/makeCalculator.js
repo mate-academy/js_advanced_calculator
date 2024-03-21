390b606a9d04cf8d4db2c1d8f4547b19e657651b
Oleksandr Goryachun <aleks.goryachun@gmail.com>
2024-03-21 19:42:56 +0200

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,57 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(num) {
+      const number = this;
+
+      number.result += num;
+
+      return number;
+    },
+
+    subtract(num) {
+      const number = this;
+
+      number.result -= num;
+
+      return number;
+    },
+
+    multiply(num) {
+      const number = this;
+
+      number.result *= num;
+
+      return number;
+    },
+
+    divide(num) {
+      const number = this;
+
+      number.result /= num;
+
+      return number;
+    },
+
+    reset() {
+      const number = this;
+
+      number.result = 0;
+
+      return number;
+    },
+
+    operate(callback, num) {
+      const number = this;
+
+      callback.call(number, num);
+
+      return number;
+    },
+  };
 }
 
 module.exports = makeCalculator;
