f3226b9817a51208aa999d909e6bae1932d1679e
Daniil Fadeev <daniilfadeev2018@gmail.com>
2024-03-26 12:32:36 +0200

    task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const obj = {
+    result: 0,
+
+    add: (value) => {
+      return (obj.result += value);
+    },
+
+    subtract: (value) => {
+      return (obj.result -= value);
+    },
+
+    multiply: (value) => {
+      return (obj.result *= value);
+    },
+
+    divide: (value) => {
+      return (obj.result /= value);
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, num) {
+      operation.call(this, num);
+
+      return this;
+    },
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
