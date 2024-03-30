3bd995873a8068e37cfd5ef0e7ea633467d73fe8
Vnxz <68802852+vinasxz@users.noreply.github.com>
2024-03-29 22:12:28 -0300

    adding my calc

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,45 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(number) {
+      this.result += number;
+
+      return this;
+    },
+
+    subtract(number) {
+      this.result -= number;
+
+      return this;
+    },
+
+    multiply(number) {
+      this.result *= number;
+
+      return this;
+    },
+
+    divide(number) {
+      this.result /= number;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(method, number) {
+      return method.call(this, number);
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
