787d15dcc58181df25a4315729d2c52c43502351
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-04-01 15:00:27 +0300

    get stuck

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,45 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    num: 0,
+
+    add(value) {
+      this.num += value;
+    },
+
+    subtract(value) {
+      this.num -= value;
+    },
+
+    divide(value) {
+      this.num /= value;
+    },
+
+    multiply(value) {
+      this.num *= value;
+    },
+
+    reset() {
+      this.num = 0;
+    },
+
+    result() {
+      return this.num;
+    },
+
+    operate(callback, value) {
+      const func = callback.bind(this, value);
+
+      func();
+
+      return this.num;
+    },
+  };
 }
 
 module.exports = makeCalculator;
+
+// const calculator = makeCalculator();
+
+// console.log(calculator.operate(calculator.add, 10));
