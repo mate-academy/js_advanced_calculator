d4f82f04208a3176d6a7fa81376bd568b8566bf0
Denys Povh <kirilloffdanza@gmail.com>
2024-04-12 16:40:34 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate(callback, operand) {
+      callback(operand);
+
+      return calculator;
+    },
+    add(num) {
+      calculator.result += num;
+
+      return calculator;
+    },
+    subtract(num) {
+      calculator.result -= num;
+
+      return calculator;
+    },
+    multiply(num) {
+      calculator.result *= num;
+
+      return calculator;
+    },
+    divide(num) {
+      calculator.result /= num;
+
+      return calculator;
+    },
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
