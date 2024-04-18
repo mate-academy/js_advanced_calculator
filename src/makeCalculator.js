8ff52098d4dc7047a88acff580eb8e7d9167f5fb
Yaroslav Halynskyi <125606535+Yaroslav65@users.noreply.github.com>
2024-04-18 18:33:18 +0200

    advanced calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -36,8 +36,49 @@
  *
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
+function makeCalculator()  {
+  return {
+    result: 0,
+
+    add: function(a, b) {
+      return a + b;
+    },
+    
+    subtract: function(a, b) {
+      return a - b;
+    },
+
+    multiply: function(a, b) {
+      return a * b;
+    },
+
+    divide: function(a, b) {
+      return a / b;
+    },
+
+    operate: function(operation, a, b) {
+      switch (operation) {
+        case this.add:
+          this.result = this.add(this.result, a, b);
+          break;
+        case this.subtract:
+          this.result = this.subtract(this.result, a, b);
+          break;
+        case this.multiply:
+          this.result = this.multiply(this.result, a, b);
+          break;
+        case this.divide:
+          this.result = this.divide(this.result, a, b);
+          break;
+      }
+      return this; 
+    },
+
+    reset: function() {
+      this.result = 0;
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
