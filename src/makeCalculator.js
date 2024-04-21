2d59aa9645e26f00e2d52d55f20e6d38b4b043e8
Stelmashchuk Pavlo <stelmaschukpavlo3@gmail.com>
2024-04-21 13:32:01 +0300

    Add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,28 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(operation, value) {
+      calculator.result = operation(value);
+
+      return calculator;
+    },
+
+    add: (value) => calculator.result + value,
+    subtract: (value) => calculator.result - value,
+    multiply: (value) => calculator.result * value,
+    divide: (value) => calculator.result / value,
+
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
