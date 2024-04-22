611582f5dd80155b00ae1dfac4082da5dc85b7a2
Stelmashchuk Pavlo <stelmaschukpavlo3@gmail.com>
2024-04-22 19:02:54 +0300

    Fix solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,15 +5,21 @@ function makeCalculator() {
     result: 0,
 
     operate(operation, value) {
-      calculator.result = operation(value);
+      this.result = operation(value);
 
-      return calculator;
+      return this;
     },
 
     add: (value) => calculator.result + value,
     subtract: (value) => calculator.result - value,
     multiply: (value) => calculator.result * value,
-    divide: (value) => calculator.result / value,
+    divide: (value) => {
+      if (value !== 0) {
+        return calculator.result / value;
+      }
+
+      return calculator.result;
+    },
 
     reset() {
       calculator.result = 0;
