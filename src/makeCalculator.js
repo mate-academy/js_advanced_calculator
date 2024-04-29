50f46d90c150aa8b82f77149c1a48905edb4f71b
Andrew Mykytiuk <mykytjyk@gmail.com>
2024-04-29 13:07:37 +0300

    add Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -17,6 +17,10 @@ function makeCalculator() {
       return this.result;
     },
     divide(num) {
+      if (num === 0) {
+        return 'Division by zero';
+      }
+
       this.result /= num;
 
       return this.result;
