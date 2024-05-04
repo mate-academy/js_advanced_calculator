7a863e2425e7b9426ddf75e0f9a143a2e0da27cc
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2024-05-04 13:54:00 +0100

    fix typo

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -28,7 +28,7 @@ function makeCalculator() {
 
     divide(a) {
       if (a === 0) {
-        throw new error('Can not divide by 0')
+        throw new Error('Can not divide by 0')
       }
       this.result /= a;
 
