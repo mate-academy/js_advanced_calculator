aef821e4afa1a405db248ad02e88c85f40ffabb4
Andrii Mykhailov <a.mikhas.m@gmail.com>
2024-05-07 16:20:52 +0300

    add a division-by-zero check

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -20,7 +20,9 @@ function makeCalculator() {
     },
 
     divide(currentResult, number) {
-      return currentResult / number;
+      if (number !== 0) {
+        return currentResult / number;
+      }
     },
 
     reset() {
