ba14dc82ab4c32853e48aa906ead65db1973733a
mate-student <166617557+mate-student@users.noreply.github.com>
2024-05-07 17:35:59 +0300

    Update makeCalculator.js

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(a) {
+      this.result += a;
+    },
+
+    subtract(a) {
+      this.result -= a;
+    },
+
+    multiply(a) {
+      this.result *= a;
+    },
+
+    divide(a) {
+      if (a !== 0) {
+        this.result /= a;
+      }
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, a) {
+      callback.call(this, a);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
