65d9a5532b4dbebfb4bffec0e1f65919442a2dc4
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-05-09 22:20:49 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      if (b <= 0) {
+        return 'ERROR';
+      }
+
+      return a / b;
+    },
+  };
 }
 
 module.exports = makeCalculator;
