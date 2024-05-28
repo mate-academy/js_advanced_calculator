48567fddf4c3f5d36bada93c8907addc563593b7
Stanislav Kushnir <florian.of.lorch@gmail.com>
2024-05-28 15:33:58 +0300

    made add-task makeCalculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const object = {
+    result: 0,
+  };
+
+  object.add = function (number) {
+    object.result += number;
+  };
+
+  object.subtract = function (number) {
+    object.result -= number;
+  };
+
+  object.multiply = function (number) {
+    object.result *= number;
+  };
+
+  object.divide = function (number) {
+    object.result /= number;
+  };
+
+  object.reset = function () {
+    object.result = 0;
+
+    return this;
+  };
+
+  object.operate = function (callback, number) {
+    callback(number);
+
+    return this;
+  };
+
+  return object;
 }
 
 module.exports = makeCalculator;
