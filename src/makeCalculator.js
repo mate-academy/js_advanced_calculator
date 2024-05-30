fe05c96aaf459e72f71e7d0bf1860192c1fcd244
IURII FEDYNA <iurii.fedyna@gmail.com>
2024-05-30 06:15:56 -0230

    git commit -m 'add task solution'

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,35 @@
  */
 function makeCalculator() {
   // write code here
+  let result = 0;
+
+  return {
+    get result() {
+      return result;
+    },
+    add(num) {
+      result += num;
+    },
+    subtract(num) {
+      result -= num;
+    },
+    multiply(num) {
+      result *= num;
+    },
+    divide(num) {
+      result /= num;
+    },
+    reset() {
+      result = 0;
+
+      return this;
+    },
+    operate(callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
