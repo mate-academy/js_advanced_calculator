4a4b96a05e9e1adc097103f606df8c9ded0ea732
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-02 16:17:18 +0300

    Created advanced calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,31 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      callback(number, this);
+
+      return this;
+    },
+    add(number, calculator) {
+      calculator.result += number;
+    },
+    subtract(number, calculator) {
+      calculator.result -= number;
+    },
+    multiply(number, calculator) {
+      calculator.result *= number;
+    },
+    divide(number, calculator) {
+      calculator.result /= number;
+    },
+  };
 }
 
 module.exports = makeCalculator;
