774d53388e2b008ff927fa13144b12d98bb84e01
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-09 16:54:02 +0300

    [Feature] added advanced calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const initialValue = 0;
+
+  const config = {
+    result: initialValue,
+
+    add(value) {
+      this.result += value;
+    },
+
+    subtract(value) {
+      this.result -= value;
+    },
+
+    multiply(value) {
+      this.result *= value;
+    },
+
+    divide(value) {
+      this.result /= value;
+    },
+
+    reset() {
+      this.result = initialValue;
+
+      return this;
+    },
+
+    operate(operation, value) {
+      operation.bind(this)(value);
+
+      return this;
+    },
+  };
+
+  return config;
 }
 
 module.exports = makeCalculator;
