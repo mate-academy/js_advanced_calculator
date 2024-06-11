4bce86da453236cf7ede9555c1b92ad27e33964e
Viktor Zarvanskyi <viktor.zarvanskyi@gmail.com>
2024-06-11 20:21:19 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(operand) {
+      this.result += operand;
+
+      return this;
+    },
+    subtract(operand) {
+      this.result -= operand;
+
+      return this;
+    },
+    multiply(operand) {
+      this.result *= operand;
+
+      return this;
+    },
+    divide(operand) {
+      if (operand !== 0) {
+        this.result /= operand;
+      }
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
