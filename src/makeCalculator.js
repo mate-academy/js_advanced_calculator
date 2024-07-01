d39b3e5307095b7e10a1279f4019238cb05a58e9
haduigon <haduigon@gmail.com>
2024-07-01 18:58:24 +0300

    passing this tas again

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,12 +5,12 @@
  * Create a `makeCalculator` function that returns an object that
  * has the following fields:
  *  - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
- *  - The `total` property is initially 0.
+ *  - The `result` property is initially 0.
  *
  * How the calculator will work:
  * - Each `operate` call takes a callback and a value and sets the
- *   appropriate value to the `total` property.
- * - The `reset` method resets `total` value to 0.
+ *   appropriate value to the `result` property.
+ * - The `reset` method resets `result` value to 0.
  * - `add`, `subtract`, `multiply`, `divide` are passed as callbacks to
  *   `operate` method
  * - The `operate` and `reset` methods can be called in a chain.
@@ -19,10 +19,10 @@
  * const calculator = makeCalculator();
  *
  * calculator.operate(calculator.add, 21)
- * calculator.total === 21
+ * calculator.result === 21
 
  * calculator.reset()
- * calculator.total === 0
+ * calculator.result === 0
  * calculator
  *  .operate(calculator.add, 10)
  *  .reset()
@@ -30,39 +30,39 @@
  *  .operate(calculator.divide, 5)
  *  .operate(calculator.multiply, 7)
  *
- * calculator.total === -28
+ * calculator.result === -28
  *
  *
  * @return {object}
  */
 function makeCalculator() {
   const calculator = {
-    total: 0,
+    result: 0,
     operate: function(callback, callbackValue) {
       callback(callbackValue);
 
       return this;
     },
     add: function(value) {
-      calculator.total += value;
+      calculator.result += value;
 
       return this;
     },
     subtract: function(value) {
-      calculator.total = calculator.total - Math.abs(value);
+      calculator.result = calculator.result - Math.abs(value);
 
       return this;
     },
     multiply: function(value) {
-      calculator.total = calculator.total * value;
+      calculator.result = calculator.result * value;
     },
     divide: function(value) {
-      calculator.total = calculator.total / value;
+      calculator.result = calculator.result / value;
 
       return this;
     },
     reset: function() {
-      calculator.total = 0;
+      calculator.result = 0;
 
       return this;
     },
