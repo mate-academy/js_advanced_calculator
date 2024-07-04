213bf0f146513d6d92849ef5555d1b4c5a6eaf7d
SerhiiSemennikov <semennik@hotmail.com>
2024-07-04 15:33:40 +0200

    new

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,44 @@
  */
 function makeCalculator() {
   // write code here
+
+  return {
+    result: 0,
+    add(value) {
+      this.result += value;
+
+      return this;
+    },
+    subtract(value) {
+      this.result -= value;
+
+      return this;
+    },
+    multiply(value) {
+      this.result *= value;
+
+      return this;
+    },
+    divide(value) {
+      if (value === 0) {
+        return 'error';
+      } else {
+        this.result /= value;
+      }
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
