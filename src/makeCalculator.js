a3fc972bcd4a360721a4a1193ad9121fd33e76bd
Roman Bekh <coldshein@gmail.com>
2024-07-09 00:02:10 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,37 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, value) {
+      this.result = operation(this.result, value);
+
+      return this;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    divide(a, b) {
+      return a / b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+  };
 }
 
 module.exports = makeCalculator;
