14ff47abae309db2564ac846ffbfcd80e2624017
Olia Shymanska <oliaa.shymanska@gmail.com>
2024-07-09 13:57:29 +0300

    make calculate advanced

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate(callback, operand) {
+      this.result = callback(operand, this.result);
+
+      return this;
+    },
+
+    add(operand, result) {
+      return result + operand;
+    },
+
+    subtract(operand, result) {
+      return result - operand;
+    },
+
+    multiply(operand, result) {
+      return result * operand;
+    },
+
+    divide(operand, result) {
+      return result / operand;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
