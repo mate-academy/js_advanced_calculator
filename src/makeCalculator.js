e6176add2e9400e3b40bcad2a903591fe6713b38
Dmitriy Matvichuk <dmytromatviichuk1979@gmail.com>
2024-07-13 18:43:31 +0300

    Done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,34 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add: function (number) {
+      this.result += number;
+    },
+    subtract: function (number) {
+      this.result -= number;
+    },
+    multiply: function (number) {
+      this.result *= number;
+    },
+    divide: function (number) {
+      this.result /= number;
+    },
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function (callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
