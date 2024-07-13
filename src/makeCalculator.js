87fcfb3c2fc3f693550558eb71f474c0d447faee
Roma Zuravliov <janmorozov1988@gmail.com>
2024-07-13 10:27:49 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,51 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  function operate(callback, number) {
+    result = callback(result, number);
+
+    return this;
+  }
+
+  function reset() {
+    result = 0;
+
+    return this;
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    if (b !== 0) {
+      return a / b;
+    } else {
+      throw new Error('Division by zero');
+    }
+  }
+
+  return {
+    get result() {
+      return result;
+    },
+    operate,
+    reset,
+    add,
+    subtract,
+    multiply,
+    divide,
+  };
 }
 
 module.exports = makeCalculator;
