c929bd83c9f58ffd9d55f33e8dea0c3243ae1234
Anya Laban <laban.anya.a@gmail.com>
2024-07-17 10:16:26 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,23 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(action, number) {
+      this.result = action(this.result, number);
+
+      return this;
+    },
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+  };
 }
 
 module.exports = makeCalculator;
