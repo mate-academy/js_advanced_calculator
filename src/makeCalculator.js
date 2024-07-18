8a0505ffa520ee42bb6a27f99d9e029ea8266d0b
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-07-18 23:15:10 +0200

    everything is completed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,35 @@
 /**
  * @return {object}
  */
+
+// `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`
+
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add(val) {
+      this.result += val;
+    },
+    subtract(val) {
+      this.result -= val;
+    },
+    multiply(val) {
+      this.result *= val;
+    },
+    divide(val) {
+      this.result /= val;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(operation, val) {
+      operation.call(this, val);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
