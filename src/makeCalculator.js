804f9d4a54fa87f3757d5d5b305b39c22c4690b9
Igor <igor.olshanskyi@gmail.com>
2024-07-18 16:18:05 +0300

    solve

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,41 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+    add(x) {
+      this.result += x;
+
+      return this;
+    },
+    subtract(x) {
+      this.result -= x;
+
+      return this;
+    },
+    multiply(x) {
+      this.result *= x;
+
+      return this;
+    },
+    divide(x) {
+      this.result /= x;
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function (callbak, x) {
+      callbak.call(this, x);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
