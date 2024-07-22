f4ffd69ebebfc2f85c2caadba52abb350e585d26
Anton Donchenko <donch.a080@gmail.com>
2024-07-22 10:30:48 +0300

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate(action, number) {
+      this.result = action(this.result, number);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    add(currentResult, number) {
+      return currentResult + number;
+    },
+    subtract(currentResult, number) {
+      return currentResult - number;
+    },
+    multiply(currentResult, number) {
+      return currentResult * number;
+    },
+    divide(currentResult, number) {
+      return currentResult / number;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
