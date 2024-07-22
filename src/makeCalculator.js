b04c272d6be34868d203b93a26c02f174fa007ec
Tanya Baletska <t.baletska@gmail.com>
2024-07-22 05:54:05 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(num) {
+      this.result += num;
+    },
+    subtract(num) {
+      this.result -= num;
+    },
+    multiply(num) {
+      this.result *= num;
+    },
+    divide(num) {
+      if (num !== 0) {
+        this.result /= num;
+      } else {
+        throw new Error('Error!');
+      }
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(fn, num) {
+      fn.call(this, num);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
