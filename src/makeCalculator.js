f8eb9df1da6dffe54d3985d4ee10e247b5f43b08
George <george.boujhenitsa@gmail.com>
2024-07-25 16:05:24 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,30 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const abilities = {
+    result: 0,
+    add: (number) => (abilities.result += number),
+    subtract: (number) => (abilities.result -= number),
+    multiply: (number) => (abilities.result *= number),
+    divide: (number) => (abilities.result /= number),
+
+    operate: operate,
+    reset: reset,
+  };
+
+  function operate(callback, number) {
+    callback(number);
+
+    return abilities;
+  }
+
+  function reset() {
+    abilities.result = 0;
+
+    return abilities;
+  }
+
+  return abilities;
 }
 
 module.exports = makeCalculator;
