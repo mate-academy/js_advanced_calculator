b7b50f0c40c6107a26527ae7270c71f54fb87157
Marian <getmoodyup@gmail.com>
2024-08-02 12:33:29 +0300

    linter

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -6,10 +6,10 @@
 function makeCalculator() {
   const calc = {
     result: 0,
-    add: (n) => calc.result += n,
-    subtract: (n) => calc.result -= n,
-    multiply: (n) => calc.result *= n,
-    divide: (n) => calc.result /= n,
+    add: (n) => (calc.result += n),
+    subtract: (n) => (calc.result -= n),
+    multiply: (n) => (calc.result *= n),
+    divide: (n) => (calc.result /= n),
 
     operate: function (callback, number) {
       callback(number);
@@ -17,8 +17,8 @@ function makeCalculator() {
       return this;
     },
 
-    reset: () => calc.result = 0,
-  }
+    reset: () => (calc.result = 0),
+  };
 
   return calc;
 }
