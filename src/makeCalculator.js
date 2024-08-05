85cf91ab57aa106da011e732e9de250b49d40aa3
ansnihur <snihur.anastasiia@gmail.com>
2024-08-05 23:04:33 +0300

    solution added

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,37 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 /**
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add: function (value) {
+      this.result += value;
+    },
+    subtract: function (value) {
+      this.result -= value;
+    },
+    multiply: function (value) {
+      this.result *= value;
+    },
+    divide: function (value) {
+      if (value !== 0) {
+        this.result /= value;
+      }
+    },
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function (callback, value) {
+      callback.call(this, value);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
