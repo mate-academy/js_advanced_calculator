6bba6b2e3e9a9360549f67a3bc95ab62d6b2cbdf
vanvalera <115824927+vanvalera@users.noreply.github.com>
2024-08-05 15:49:37 +0300

    completed task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    operate(callback, number) {
+      result = callback(result, number);
+
+      return this;
+    },
+    reset() {
+      result = 0;
+
+      return this;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
