d58941fa9cbea56befef41e249c2dcbcf083e6fe
Mykhailo Firka <ltcar@online.ua>
2024-08-11 23:25:42 +0300

    function makeCalculator is created

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,49 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    get result() {
+      return result;
+    },
+
+    add: (num) => {
+      result += num;
+
+      return this;
+    },
+
+    subtract: (num) => {
+      result -= num;
+
+      return this;
+    },
+
+    multiply: (num) => {
+      result *= num;
+
+      return this;
+    },
+
+    divide: (num) => {
+      result /= num;
+
+      return this;
+    },
+
+    reset() {
+      result = 0;
+
+      return this;
+    },
+
+    operate(operation, num) {
+      operation.call(this, num);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
