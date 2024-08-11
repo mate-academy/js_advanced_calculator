06b248d7898f69c4d6e72a91904afd3793624684
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-08-11 18:25:51 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,47 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const obj = {
+    result: 0,
+
+    operate(operation, num) {
+      operation(num);
+
+      return obj;
+    },
+
+    add(x) {
+      obj.result += x;
+
+      return obj;
+    },
+
+    subtract(x) {
+      obj.result -= x;
+
+      return obj;
+    },
+
+    divide(x) {
+      obj.result /= x;
+
+      return obj;
+    },
+
+    multiply(x) {
+      obj.result *= x;
+
+      return obj;
+    },
+
+    reset() {
+      obj.result = 0;
+
+      return obj;
+    },
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
