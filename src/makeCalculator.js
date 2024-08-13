c9c88476bb8c3292b18a6d346a5c446cab3c0de5
Anna Agerone <olesnevych12@gmail.com>
2024-08-13 12:54:47 -0500

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,38 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    add(number) {
+      this.result += number;
+    },
+    subtract(number) {
+      this.result -= number;
+    },
+    multiply(number) {
+      this.result *= number;
+    },
+    divide(number) {
+      if (number === 0) {
+        throw new Error('Error');
+      }
+
+      this.result /= number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
