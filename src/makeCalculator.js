39c16a96a861497eda0c4a1829feb814407024c9
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-13 11:22:04 +0300

    calculator solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,36 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(num) {
+      calculator.result += num;
+    },
+    subtract(num) {
+      calculator.result -= num;
+    },
+    multiply(num) {
+      calculator.result *= num;
+    },
+    divide(num) {
+      calculator.result /= num;
+    },
+
+    reset() {
+      calculator.result = 0;
+
+      return calculator; // Allow chaining
+    },
+
+    operate(callback, num) {
+      callback(num);
+
+      return calculator; // Allow chaining
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
