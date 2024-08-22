0b322a6a0f1c2643f7e43b3916ca30a2a6126c46
Tatiana Makohon (Palamarchuk) <tatiana@MacBook-Pro-Tatiana.local>
2024-08-22 18:03:43 +0300

    Check

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,42 @@
 /**
  * @return {object}
  */
-function makeCalculator() {
+function makeCalculator(operation, value) {
   // write code here
+
+  const calculator = {
+    operate(callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+
+    result: 0,
+
+    add: function (number) {
+      this.result += number;
+    },
+
+    subtract: function (number) {
+      this.result -= number;
+    },
+
+    divide: function (number) {
+      this.result /= number;
+    },
+
+    multiply: function (number) {
+      this.result *= number;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
