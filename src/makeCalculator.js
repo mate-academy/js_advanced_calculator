8c0d88ee6a8f053f2c09c0f6e4055dca11675898
siriu <kseniia.krytska@zagroza.agency>
2024-08-23 21:22:46 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,30 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const result = {
+    result: 0,
+    add(digit) {
+      return (result.result += digit);
+    },
+    subtract(digit) {
+      return (result.result -= digit);
+    },
+    multiply(digit) {
+      return (result.result *= digit);
+    },
+    divide(digit) {
+      return (result.result /= digit);
+    },
+    reset() {
+      result.result = 0;
+      return this;
+    },
+    operate(callback, number) {
+      result.result = callback(number);
+      return this;
+    },
+  };
+  return result;
 }
 
 module.exports = makeCalculator;
