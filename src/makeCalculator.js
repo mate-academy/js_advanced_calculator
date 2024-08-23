f84cf3bdd23f1280282e9233ecb3f6e0efff1341
Sasha Popovych <sashapopovych98@gmail.com>
2024-08-23 13:43:11 +0200

    test

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add: function (num) {
+      return this.result + num;
+    },
+    subtract: function (num) {
+      return this.result - num;
+    },
+    multiply: function (num) {
+      return this.result * num;
+    },
+    divide: function (num) {
+      return this.result / num;
+    },
+    reset: function (num) {
+      this.result = 0;
+
+      return this;
+    },
+    operate: function (callback, num) {
+      this.result = callback.call(this, num);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
