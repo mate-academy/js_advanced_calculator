cde68cbb2ea87ca35ecb0f1754394bab494710f7
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-08-27 20:32:36 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    add(value) {
+      result += value;
+    },
+
+    subtract(value) {
+      result -= value;
+    },
+
+    multiply(value) {
+      result *= value;
+    },
+
+    divide(value) {
+      if (value !== 0) {
+        result /= value;
+      }
+    },
+
+    reset(value) {
+      result = 0;
+
+      return this;
+    },
+
+    operate(operation, value) {
+      operation(value);
+
+      return this;
+    },
+
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
