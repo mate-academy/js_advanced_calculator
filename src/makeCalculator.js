9c8a45080d74cc98287a1dd0bf210f51b3154dee
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-08-31 16:41:17 +0300

    done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,45 @@
  */
 function makeCalculator() {
   // write code here
+  const calculator = {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+
+      return this;
+    },
+
+    subtract(value) {
+      this.result -= value;
+
+      return this;
+    },
+
+    multiply(value) {
+      this.result *= value;
+
+      return this;
+    },
+
+    divide(value) {
+      this.result /= value;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, value) {
+      return operation.call(this, value);
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
