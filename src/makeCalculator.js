54c3df7770e2c52801e0f4f7a76135697f817836
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2024-08-31 19:08:34 +0300

    add calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(callbackOperation, number) {
+      callbackOperation(number, this);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(num, obj) {
+      obj.result += num;
+    },
+
+    subtract(num, obj) {
+      obj.result -= num;
+    },
+
+    multiply(num, obj) {
+      obj.result *= num;
+    },
+
+    divide(num, obg) {
+      obg.result /= num;
+    },
+  };
 }
 
 module.exports = makeCalculator;
