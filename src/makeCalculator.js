205d258a0badc1231f1b37d52f2ae2d6a35fb82f
missk <rostock.vasiljev@gmail.com>
2024-09-08 22:49:27 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,47 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    add(value){
+      result += value;
+      return this;
+    },
+
+    subtract(value){
+      result -= value;
+      return this;
+    },
+
+    multiply(value){
+      result *= value;
+      return this;
+    },
+
+    divide(value){
+      if(value !== 0){
+        result /= value;
+        return this;
+      } else {
+        console.log('Error: Division by zero');
+      }
+    },
+
+    reset(){
+      result = 0;
+      return this;
+    },
+
+    operate(operation, value){
+      operation.call(this, value);
+      return this;
+    },
+
+    get result(){
+      return result;
+    }
+  };
 }
 
 module.exports = makeCalculator;
