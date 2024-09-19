b2d6efb7f2851535750a7da2fefe79c8c95efb08
Oleksii Karpov <alneon1@gmail.com>
2024-09-19 11:36:49 +0300

    added solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,39 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  const resultObj = {
+    result: 0,
+
+    add(number) {
+      resultObj.result += number;
+    },
+
+    subtract(number) {
+      resultObj.result -= number;
+    },
+
+    multiply(number) {
+      resultObj.result *= number;
+    },
+
+    divide(number) {
+      resultObj.result /= number;
+    },
+
+    operate(callback, number) {
+      callback(number);
+
+      return this;
+    },
+
+    reset() {
+      resultObj.result = 0;
+
+      return this;
+    },
+  };
+
+  return resultObj;
 }
 
 module.exports = makeCalculator;
