61461e85f5426c253a8956c5c0a092a59bbe3bb4
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2024-09-25 13:00:57 +0300

    task was solved

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -2,9 +2,34 @@
 
 /**
  * @return {object}
+ * - Methods: `add`, `subtract`, `multiply`, `divide`, `reset`, `operate`.
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    add(y, x) {
+      return y + x;
+    },
+    subtract(y, x) {
+      return y - x;
+    },
+    multiply(y, x) {
+      return y * x;
+    },
+    divide(y, x) {
+      return y / x;
+    },
+    operate(callback, param) {
+      this.result = callback(this.result, param);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
