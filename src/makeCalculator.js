fe5542349c2d7f95ea06e9d584e4eeb679c4a0ab
Halyna <minimal005@gmail.com>
2024-09-30 18:29:23 +0300

    my solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,37 @@
 'use strict';
 
-/**
+/*
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calc = {
+    result: 0,
+
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(action, number) {
+      this.result = action(this.result, number);
+
+      return this;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
