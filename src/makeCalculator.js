d9a74f03d0c97a4c5f1a086b093fa6b38471bf61
Vitalii Kredinets <kredinec.v@gmail.com>
2024-09-30 21:08:21 +0300

    solve

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,29 @@
  */
 function makeCalculator() {
   // write code here
+  let result = 0;
+
+  const calculator = {
+    add: (num) => result + num,
+    subtract: (num) => result - num,
+    divide: (num) => result / num,
+    multiply: (num) => result * num,
+    operate: function (callback, num) {
+      result = callback(num);
+
+      return this;
+    },
+    reset: function () {
+      result = 0;
+
+      return this;
+    },
+    get result() {
+      return result;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
