91b75de1e2af554700a2f8a2561eae81664b39dc
Aleksandr Shramchenko <mostwar1988@gmail.com>
2024-10-01 10:52:31 +0300

    test

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,71 @@
 /**
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
+function makeCalculator(operation, x) {
+  const a = x;
+  const result = 0;
+
+  makeCalculator.add = (b) => {
+    return result + b;
+  };
+
+  makeCalculator.subtract = (b) => {
+    return result - b;
+  };
+
+  makeCalculator.divide = (b) => {
+    return result / b;
+  };
+
+  makeCalculator.multiply = (b) => {
+    return result * b;
+  };
+
+  makeCalculator.reset = (res = 0) => {
+    return res;
+  };
+
+  // makeCalculator.operate = (res) => {
+  //   return console.log(res);
+  // };
+
+  return operation(a);
+
+  // function reset() {
+  //   return (result = 0);
+  // }
+
+  // function add(x) {
+  //   let b = 0;
+
+  //   b += x;
+
+  //   return b;
+  // }
+
+  // function subtract(x) {
+  //   let b = 0;
+
+  //   b -= x;
+
+  //   return b;
+  // }
+
+  // function multiply(x) {
+  //   let b = 0;
+
+  //   b *= x;
+
+  //   return b;
+  // }
+
+  // function divide(x) {
+  //   let b = 0;
+
+  //   b /= x;
+
+  //   return b;
+  // }
 }
 
 module.exports = makeCalculator;
