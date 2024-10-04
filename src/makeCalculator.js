474570c9b9b46c0906146ec294c004b9a38d27f0
SofiiaZhmurko <sofiiak209@gmail.com>
2024-10-04 19:56:19 +1300

    Add Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,48 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(num) {
+      this.result += num;
+
+      return this;
+    },
+
+    subtract(num) {
+      this.result -= num;
+
+      return this;
+    },
+
+    multiply(num) {
+      this.result *= num;
+
+      return this;
+    },
+
+    divide(num) {
+      if (num === 0) {
+        throw new Error('Error: Division by zero');
+      }
+      this.result /= num;
+
+      return this;
+    },
+
+    operate(callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
