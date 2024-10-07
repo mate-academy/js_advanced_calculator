8e2552860791c25cea8244cbd7ca4b0d704239fc
unknown <yuli4ka.lashch98@gmail.com>
2024-10-07 19:23:30 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,53 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    get result() {
+      return result;
+    },
+
+    add(value) {
+      result += value;
+
+      return this;
+    },
+
+    subtract(value) {
+      result -= value;
+
+      return this;
+    },
+
+    multiply(value) {
+      result *= value;
+
+      return this;
+    },
+
+    divide(value) {
+      if (value !== 0) {
+        result /= value;
+
+        return this;
+      }
+    },
+
+    reset() {
+      result = 0;
+
+      return this;
+    },
+
+    operate(callback, value) {
+      if (typeof callback === 'function') {
+        callback.call(this, value);
+      }
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
