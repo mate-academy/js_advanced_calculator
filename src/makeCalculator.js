e34c9c3d30d19a2be5e8385000b755901c8d7e3c
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2024-12-12 13:32:19 +0000

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,43 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(x) {
+      return this.result + x;
+    },
+
+    subtract(x) {
+      return this.result - x;
+    },
+
+    multiply(x) {
+      return this.result * x;
+    },
+
+    divide(x) {
+      if (x === 0) {
+        throw new Error('Division by zero is not allowed.');
+      }
+
+      return this.result / x;
+    },
+
+    operate(callback, value) {
+      this.result = callback.call(this, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
