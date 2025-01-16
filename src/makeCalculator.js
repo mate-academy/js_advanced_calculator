0f5232080d24b390d56dd007f7eaceae4e8400c2
g.palyukh <g.palyukh@gmail.com>
2025-01-16 19:16:36 +0100

    implement calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,36 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(number) {
+      calculator.result += number;
+    },
+    subtract(number) {
+      calculator.result -= number;
+    },
+    multiply(number) {
+      calculator.result *= number;
+    },
+    divide(number) {
+      calculator.result /= number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return calculator;
+    },
+
+    operate(operation, operand) {
+      operation(operand);
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
