1bddf7f1a63e3ba9639a3ae9678d606ad1420c62
David Moskalenko <dvdmsk21@gmail.com>
2025-02-09 23:13:17 +0200

    calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,25 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate: (operation, value) => {
+      calculator.result = operation(calculator.result, value);
+
+      return calculator;
+    },
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b,
+    reset: () => {
+      calculator.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
