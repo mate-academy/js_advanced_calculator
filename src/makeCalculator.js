83550c01d6a05558e88c869489ba34284485c435
radykhovska <zagnibeda@i.ua>
2025-02-17 19:26:43 +0200

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,39 @@
 /**
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      return a / b;
+    },
+
+    operate(operator, number) {
+      this.result = operator(this.result, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
