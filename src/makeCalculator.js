a5331c541f80abcebcc700165f8c230d43f77654
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-02-17 16:55:22 +0200

    'Solution'

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,32 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(current, number) {
+      return current + number;
+    },
+    subtract(current, number) {
+      return current - number;
+    },
+    multiply(current, number) {
+      return current * number;
+    },
+    divide(current, number) {
+      return current / number;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
