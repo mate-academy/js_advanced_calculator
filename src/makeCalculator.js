8b7462d8e5f040de516e3b2515b899ab75379aae
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-02-18 13:06:44 +0100

    calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,52 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+
+      return this;
+    },
+
+    subtract(value) {
+      this.result -= value;
+
+      return this;
+    },
+
+    multiply(value) {
+      this.result *= value;
+
+      return this;
+    },
+
+    divide(value) {
+      if (value === 0) {
+        this.result = 'Cannot divide by zero';
+
+        return this;
+      }
+      this.result /= value;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, value) {
+      callback.call(this, value);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
