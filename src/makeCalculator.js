650c9c79d52f56cc08de98bac2bede799f090e2a
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-22 20:02:13 +0100

    complete task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(callback, n) {
+      this.result = callback(this.result, n);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      if (b) {
+        return a / b;
+      }
+    },
+  };
 }
 
 module.exports = makeCalculator;
