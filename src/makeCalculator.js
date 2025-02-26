42c5b3ff20936efa0733be1bcace8b6218bd5a6d
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2025-02-26 15:01:57 +0200

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,31 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    // Математичні операції у вигляді callback-функцій
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+
+    // Операція, яка використовує callback
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this; // Повертаємо this для chain-виклику
+    },
+
+    // Скидання значення result
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
