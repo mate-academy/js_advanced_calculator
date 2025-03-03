d722f4d5e70ebd59abfc179c8c5cc399f532e739
MorelSami <mksami237@gmail.com>
2025-03-03 13:39:07 -0600

    test

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,31 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+    add: function(value) {
+       this.result += value;
+    },
+    subtract: function(value) {
+      this.result -= value;
+    },
+    multiply: function(value) {
+     this.result *= value;
+    },
+    divide: function(value) {
+     this.result /= value;
+    },
+    reset: function() {
+     this.result = 0;
+     return this;
+    },
+    operate: function(callback, num) {
+    /* eslint no-console: ["error", { allow: ["warn", "log"] }] */
+    console.log(this.result, callback, num)
+     this.callback(num);
+     return this;
+    }
+  }
 }
 
 module.exports = makeCalculator;
