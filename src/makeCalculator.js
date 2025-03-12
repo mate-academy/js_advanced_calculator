8622cacaa5aa1e78d9d0a1c6285a31395591e244
dsliusarchyk <d.divad@icloud.com>
2025-03-12 15:00:45 -0700

    done task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,24 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => (b !== 0 ? a / b : 0),
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+      return this; // Для підтримки чейнінгу
+    },
+
+    reset() {
+      this.result = 0;
+      return this; // Для підтримки чейнінгу
+    },
+  };
 }
 
 module.exports = makeCalculator;
