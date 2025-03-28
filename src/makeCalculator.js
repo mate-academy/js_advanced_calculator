3818b45c75e8339d02bc9eeeb7035f6119396137
sirserg <sirserg05@gmail.com>
2025-03-28 15:03:06 +0200

    feat: add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(result, value) {
+      return result + value;
+    },
+    subtract(result, value) {
+      return result - value;
+    },
+    multiply(result, value) {
+      return result * value;
+    },
+    divide(result, value) {
+      return result / value;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(operation, value) {
+      this.result = operation(this.result, value);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
