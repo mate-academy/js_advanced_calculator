82bc60f71930d07ae6c958ba7407df376893868e
Csillag61 <csillagok@gmail.com>
2025-03-28 16:42:54 -0400

    calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,26 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this; // Allow method chaining
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this; // Allow method chaining
+    },
+  };
 }
 
 module.exports = makeCalculator;
