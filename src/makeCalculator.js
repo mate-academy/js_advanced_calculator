2c5679801aafb6748f45dbde528c7129db935f40
maxmodrr <maxmodrr@gmail.com>
2025-04-23 14:22:53 +0300

    results

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,38 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let state = 0;
+
+  return {
+    get result() {
+      return state;
+    },
+
+    add(value) {
+      state += value;
+    },
+    subtract(value) {
+      state -= value;
+    },
+    divide(value) {
+      state /= value;
+    },
+    multiply(value) {
+      state *= value;
+
+      return this;
+    },
+    reset() {
+      state = 0;
+
+      return this;
+    },
+    operate(callback, value) {
+      callback(value);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
