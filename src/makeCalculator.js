313b5ed70e75f41490a1eae0467f4c90c4acf872
Vitalii Storcheus <storcheusvitalii@gmail.com>
2025-04-29 21:39:50 +0300

    calculate

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,42 @@
 /**
  * @return {object}
  */
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
 function makeCalculator() {
-  // write code here
+  const calculate = {
+    result: 0,
+    add,
+    subtract,
+    multiply,
+    divide,
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculate;
 }
 
 module.exports = makeCalculator;
