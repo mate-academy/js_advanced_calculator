0b9b9a621c0b001b1363134307beb3544316e6a2
Lucas Mendes <luckajw@gmail.com>
2025-05-16 12:53:52 -0300

    resolução da tarefa calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,31 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add(num) {
+      return this.result + num;
+    },
+    subtract(num) {
+      return this.result - num;
+    },
+    multiply(num) {
+      return this.result * num;
+    },
+    divide(num) {
+      return this.result / num;
+    },
+    operate(callback, num) {
+      this.result = callback.call(this, num); // Usa o callback no `result`
+
+      return this; // Permite chaining
+    },
+    reset() {
+      this.result = 0;
+
+      return this; // Permite chaining
+    },
+  };
 }
 
 module.exports = makeCalculator;
