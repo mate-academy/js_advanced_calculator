3087a6e7498412e74cf41ef263706cf23ebd568a
Alexandre Almeida <alexandredd@duck.com>
2025-05-26 17:30:05 -0300

    task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -31,18 +31,4 @@ function makeCalculator() {
   };
 }
 
-// const calculator = makeCalculator();
-
 module.exports = makeCalculator;
-
-// calculator.operate(calculator.add, 21);
-// console.log(calculator.result);
-// calculator.reset();
-// console.log(calculator.result);
-// calculator
-//   .operate(calculator.add, 10)
-//   .reset()
-//   .operate(calculator.subtract, 20)
-//   .operate(calculator.divide, 5)
-//   .operate(calculator.multiply, 7);
-// console.log(calculator.result);
