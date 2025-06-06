2065cb708695ad234f59cbf29375ac5dc20d4642
Maksym Panasovskyi <ie.panasovskyi@gmail.com>
2025-06-06 11:16:53 +0100

    added typeof check

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -8,7 +8,9 @@ function makeCalculator() {
     result: 0,
 
     operate(operation, value) {
-      operation(value);
+      if (typeof operation === 'function') {
+        operation(value);
+      }
 
       return this;
     },
