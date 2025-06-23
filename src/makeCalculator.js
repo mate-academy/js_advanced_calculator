afec0db0fec4f91d8dd7a0afe779a2f5c03e17a2
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-06-23 13:31:47 +0300

    calculator done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,31 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+    add: function (a) {
+      return this.result + a;
+    },
+    subtract: function (a) {
+      return this.result - a;
+    },
+    multiply: function (a) {
+      return this.result * a;
+    },
+    divide: function (a) {
+      return this.result / a;
+    },
+    operate: function (operation, number) {
+      this.result = operation.call(this, number);
+
+      return this;
+    },
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
