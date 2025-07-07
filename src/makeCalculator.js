95a73c66de976c8ea9a4591c82e0e4c4a9b5b45b
Ivan <kovalevivan420@gmail.com>
2025-07-07 15:00:10 +0200

    new version

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -6,51 +6,42 @@
 function makeCalculator() {
   const calculator = {
     result: 0,
-    operate(callback, x) {
-      if (callback === this.add) {
-        return this.add(x);
-      }
-
-      if (callback === this.subtract) {
-        return this.subtract(x);
-      }
-
-      if (callback === this.divide) {
-        return this.divide(x);
-      }
-
-      if (callback === this.multiply) {
-        return this.multiply(x);
-      }
-
-      if (callback === this.reset) {
-        return this.reset;
-      }
-    },
     add(x) {
       this.result += x;
 
       return this;
     },
+
     subtract(x) {
       this.result -= x;
 
       return this;
     },
+
     divide(x) {
       this.result /= x;
 
       return this;
     },
+
     multiply(x) {
       this.result *= x;
 
       return this;
     },
+
     reset() {
       this.result = 0;
 
       return this;
+    },
+
+    operate(callback, x) {
+      if (typeof callback === 'function') {
+        callback.call(this, x);
+
+        return this;
+      }
     },
   };
 
