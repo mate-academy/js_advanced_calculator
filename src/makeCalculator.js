ba0e0a0f7116c90386b7268b1de7052e1473483b
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-07-08 11:14:31 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,30 @@
  */
 function makeCalculator() {
   // write code here
+  const result = 0;
+
+  const calculator = {
+    result,
+
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
