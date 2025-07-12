55456df73067d183d0419ff684359aefa69495c2
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-12 15:29:34 +0300

    task done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,30 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let currentResult = 0;
+
+  return {
+    operate(operationFn, value) {
+      currentResult = operationFn(currentResult, value);
+
+      return this;
+    },
+
+    reset() {
+      currentResult = 0;
+
+      return this;
+    },
+
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+
+    get result() {
+      return currentResult;
+    },
+  };
 }
 
 module.exports = makeCalculator;
