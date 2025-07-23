9c8e2922808c2899823130455b7a1475b4262bc8
Nikita Chupahin <nik123chupahin@gmail.com>
2025-07-23 11:57:14 +0300

    added task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,34 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => a / b;
+
+  const calculator = {
+    get result() {
+      return result;
+    },
+    add,
+    subtract,
+    multiply,
+    divide,
+    reset() {
+      result = 0;
+
+      return calculator;
+    },
+    operate(callback, value) {
+      result = callback(result, value);
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
