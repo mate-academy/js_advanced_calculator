f8f8e10c4d64b9fc3e440ded2f17fbb58816b5be
kirky359 <lol783176@gmail.com>
2025-08-07 18:47:09 +0300

    task completed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,38 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const calculator = {
+    operate(callback, number) {
+      result = callback(number);
+
+      return this;
+    },
+    reset() {
+      result = 0;
+
+      return this;
+    },
+
+    add(number) {
+      return result + number;
+    },
+    subtract(number) {
+      return result - number;
+    },
+    divide(number) {
+      return result / number;
+    },
+    multiply(number) {
+      return result * number;
+    },
+    get result() {
+      return result;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
