7df30df54b6304251ec50f9385411d984fb3678f
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-08-07 14:20:37 +0300

    Complete task makeCalculator with 2 variants

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,62 @@
 /**
  * @return {object}
  */
+// function makeCalculator() {
+//   return {
+//     result: 0,
+//     add(value) {
+//       this.result += value;
+//     },
+//     subtract(value) {
+//       this.result -= value;
+//     },
+//     multiply(value) {
+//       this.result *= value;
+//     },
+//     divide(value) {
+//       this.result /= value;
+//     },
+//     reset() {
+//       this.result = 0;
+
+//       return this;
+//     },
+//     operate(callback, value) {
+//       callback.call(this, value);
+
+//       return this;
+//     },
+//   };
+// }
+
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add(value) {
+      calculator.result += value;
+    },
+    subtract(value) {
+      calculator.result -= value;
+    },
+    multiply(value) {
+      calculator.result *= value;
+    },
+    divide(value) {
+      calculator.result /= value;
+    },
+    reset() {
+      calculator.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      callback(number);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
