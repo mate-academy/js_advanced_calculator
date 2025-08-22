828fb0d3f881ae0442cd44846b53a13d8790516f
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-08-22 20:50:28 +0300

    implementation of advacned calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,56 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  // private properties
+  let result = 0;
+
+  // constants
+  const DIV_BY_ZERO_ERR_MSG = 'Division by zero is not allowed.';
+
+  return {
+    add(x) {
+      result += x;
+
+      return this;
+    },
+
+    subtract(x) {
+      result -= x;
+
+      return this;
+    },
+
+    multiply(x) {
+      result *= x;
+
+      return this;
+    },
+
+    divide(x) {
+      if (x === 0) {
+        throw new Error(DIV_BY_ZERO_ERR_MSG);
+      }
+      result /= x;
+
+      return this;
+    },
+
+    reset() {
+      result = 0;
+
+      return this;
+    },
+
+    operate(operatorFunc, operand) {
+      operatorFunc(operand);
+
+      return this;
+    },
+
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
