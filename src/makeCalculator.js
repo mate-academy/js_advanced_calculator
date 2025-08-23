5bc6dcfc0f148d5fe02efa99239ca3534432cb80
Sergey Zubinskiy <sergey.zubinskiy@skynix.co>
2025-08-23 15:47:18 +0300

    refactor as per AI task request

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,57 +1,91 @@
 'use strict';
 
 /**
+ * Create Calculator object
+ *
  * @return {object}
  */
 function makeCalculator() {
-  // private properties
-  let result = 0;
-
   // constants
   const DIV_BY_ZERO_ERR_MSG = 'Division by zero is not allowed.';
 
+  // return calculator object
   return {
-    add(x) {
-      result += x;
+    // current result of calculation
+    result: 0,
 
-      return this;
+    // implementation of operations
+
+    /**
+     * Add operation
+     *
+     * @param currentResult
+     * @param operand
+     * @return {*}
+     */
+    add(currentResult, operand) {
+      return currentResult + operand;
     },
 
-    subtract(x) {
-      result -= x;
-
-      return this;
+    /**
+     * Substract operation
+     *
+     * @param currentResult
+     * @param operand
+     * @return {number}
+     */
+    subtract(currentResult, operand) {
+      return currentResult - operand;
     },
 
-    multiply(x) {
-      result *= x;
-
-      return this;
+    /**
+     * Multiply operation
+     *
+     * @param currentResult
+     * @param operand
+     * @return {number}
+     */
+    multiply(currentResult, operand) {
+      return currentResult * operand;
     },
 
-    divide(x) {
-      if (x === 0) {
+    /**
+     * Divide operation
+     *
+     * @param currentResult
+     * @param operand
+     * @return {number}
+     */
+    divide(currentResult, operand) {
+      if (operand === 0) {
         throw new Error(DIV_BY_ZERO_ERR_MSG);
       }
-      result /= x;
 
-      return this;
+      return currentResult / operand;
     },
 
+    /**
+     * Returns 0, to reset current calculator acc
+     *
+     * @return {number}
+     */
     reset() {
-      result = 0;
+      this.result = 0;
 
       return this;
     },
 
-    operate(operatorFunc, operand) {
-      operatorFunc(operand);
+    /**
+     * Execute callback and modify internal result property
+     *
+     * @param operatorFunction
+     * @param operand
+     * @return {*}
+     */
+    operate(operatorFunction, operand = '') {
+      this.result = operatorFunction(this.result, operand);
 
       return this;
-    },
-
-    get result() {
-      return result;
     },
   };
 }
