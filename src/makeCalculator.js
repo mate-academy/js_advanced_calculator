b10ddfc9ee1fa0709fd3684a4e6fb39ed013da63
Artem Arseniev <144947394+Aarseniev@users.noreply.github.com>
2025-08-24 07:55:55 +0300

    updated code

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -6,43 +6,37 @@
 function makeCalculator() {
   const calculator = {
     result: 0,
-    add,
-    subtract,
-    multiply,
-    divide,
-    reset,
-    operate,
+
+    add(currentResult, additionalValue) {
+      return currentResult + additionalValue;
+    },
+
+    subtract(currentResult, additionalValue) {
+      return currentResult - additionalValue;
+    },
+
+    multiply(currentResult, additionalValue) {
+      return currentResult * additionalValue;
+    },
+
+    divide(currentResult, additionalValue) {
+      return currentResult / additionalValue;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
   };
 
   return calculator;
-}
-
-function add(currentResult, additionalValue) {
-  return currentResult + additionalValue;
-}
-
-function subtract(currentResult, additionalValue) {
-  return currentResult - additionalValue;
-}
-
-function multiply(currentResult, additionalValue) {
-  return currentResult * additionalValue;
-}
-
-function divide(currentResult, additionalValue) {
-  return currentResult / additionalValue;
-}
-
-function reset() {
-  this.result = 0;
-
-  return this;
-}
-
-function operate(callback, number) {
-  this.result = callback(this.result, number);
-
-  return this;
 }
 
 module.exports = makeCalculator;
