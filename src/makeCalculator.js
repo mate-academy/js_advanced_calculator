264fcb4a0e08c4cc68455725339a225645c1e5e0
Boris Mund <boris17527684@gmail.com>
2025-09-01 21:41:18 +0300

    add calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,25 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const obj = {
+    result: 0,
+    add: (curr, num) => curr + num,
+    divide: (curr, num) => curr / num,
+    subtract: (curr, num) => curr - num,
+    multiply: (curr, num) => curr * num,
+    operate(task, numb) {
+      this.result = task(this.result, numb);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
