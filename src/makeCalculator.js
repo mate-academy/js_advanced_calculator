9c4fa466621bc22ab5cec3fd8d7ea9078d3f2edf
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-07 22:46:26 -0300

    development of addvanced calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,61 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  // Define operation callbacks
+  function add(x) {
+    return result + x;
+  }
+
+  function subtract(x) {
+    return result - x;
+  }
+
+  function multiply(x) {
+    return result * x;
+  }
+
+  function divide(x) {
+    if (x === 0) throw new Error("Cannot divide by zero");
+    return result / x;
+  }
+
+  return {
+    get result() {
+      return result;
+    },
+    operate(callback, value) {
+      result = callback(value);
+      return this; // Enable chaining
+    },
+    reset() {
+      result = 0;
+      return this; // Enable chaining
+    },
+    add,
+    subtract,
+    multiply,
+    divide
+  };
 }
+
+const calculator = makeCalculator();
+
+calculator.operate(calculator.add, 21)
+
+console.log(calculator.result); // 21
+
+calculator.reset()
+console.log(calculator.result); // 0
+
+calculator
+  .operate(calculator.add, 10)
+  .reset()
+  .operate(calculator.subtract, 20)
+  .operate(calculator.divide, 5)
+  .operate(calculator.multiply, 7)
+
+console.log(calculator.result); // -28
 
 module.exports = makeCalculator;
