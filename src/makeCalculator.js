fc3fdff0cc6cb1a241e3bc70796e56a93309ec20
Nazar Kobernyk <nazarkoko@icloud.com>
2025-10-09 10:54:01 +0100

    task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      if (b === 0) {
+        throw new Error('Division by zero.');
+      }
+
+      return a / b;
+    },
+    operate(callback, n) {
+      this.result = callback(this.result, n);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
