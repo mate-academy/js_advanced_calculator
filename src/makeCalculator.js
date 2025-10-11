e0ca84a74423b33426ad98aaf7f962fa4e44e177
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-11 19:03:19 +0200

    Dodano funkcję makeCalculator z wszystkimi metodami

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,37 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+    },
+
+    subtract(value) {
+      this.result -= value;
+    },
+
+    multiply(value) {
+      this.result *= value;
+    },
+
+    divide(value) {
+      if (value === 0) {
+        throw new Error('Nie można dzielić przez zero!');
+      }
+      this.result /= value;
+    },
+
+    reset() {
+      this.result = 0;
+      return this; // pozwala na chainowanie
+    },
+
+    operate(callback, value) {
+      callback.call(this, value);
+      return this; // pozwala na chainowanie
+    }
+  };
 }
 
-module.exports = makeCalculator;
