637dcda96e69704d9bd04c505e98432d680af93f
Vladyslav <vkobeluh@gmail.com>
2025-10-15 14:39:11 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate: function (operation, number) {
+      this.result = operation(this.result, number);
+
+      return this;
+    },
+
+    add: function (a, b) {
+      return a + b;
+    },
+
+    subtract: function (a, b) {
+      return a - b;
+    },
+
+    multiply: function (a, b) {
+      return a * b;
+    },
+
+    divide: function (a, b) {
+      return a / b;
+    },
+
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
