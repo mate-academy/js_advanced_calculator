e3a9793bc8c313f302d9421b8b6b4d22b93285cf
Vladyslav <vkobeluh@gmail.com>
2025-10-16 14:25:43 +0300

    fix solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -8,25 +8,25 @@ function makeCalculator() {
     result: 0,
 
     operate(operation, number) {
-      this.result = operation(this.result, number);
+      operation.call(this, number);
 
       return this;
     },
 
-    add(a, b) {
-      return a + b;
+    add(number) {
+      this.result += number;
     },
 
-    subtract(a, b) {
-      return a - b;
+    subtract(number) {
+      this.result -= number;
     },
 
-    multiply(a, b) {
-      return a * b;
+    multiply(number) {
+      this.result *= number;
     },
 
-    divide(a, b) {
-      return a / b;
+    divide(number) {
+      this.result /= number;
     },
 
     reset() {
