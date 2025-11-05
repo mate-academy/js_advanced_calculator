cba0d9f5d7581fb21d45d6bf52b5d877ce330004
Vasyl Vorontsov <voronvasia@gmail.com>
2025-11-05 22:18:15 +0200

    task completed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,43 @@
 /**
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(operation, value) {
+      this.result = operation(this.result, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(a, b) {
+      return a + b;
+    },
+
+    subtract(a, b) {
+      return a - b;
+    },
+
+    multiply(a, b) {
+      return a * b;
+    },
+
+    divide(a, b) {
+      if (b === 0) {
+        throw new Error('Division by zero');
+      }
+
+      return a / b;
+    },
+  };
 }
 
 module.exports = makeCalculator;
