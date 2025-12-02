69975cf9c495a88454f4e258eae1136669978be6
Olga Betsenko <betsenkoo@gmail.com>
2025-12-02 19:04:16 +0200

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(callback, number) {
+      this.result = callback(this.result, number);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(number) {
+      return this.operate((a, b) => a + b, number);
+    },
+
+    subtract(number) {
+      return this.operate((a, b) => a - b, number);
+    },
+
+    multiply(number) {
+      return this.operate((a, b) => a * b, number);
+    },
+
+    divide(number) {
+      return this.operate((a, b) => a / b, number);
+    },
+  };
 }
 
 module.exports = makeCalculator;
