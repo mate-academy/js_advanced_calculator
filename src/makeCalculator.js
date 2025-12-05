c5f54e81264e8aa640a2b310cd84c54a05952334
Evgenu <bogdanovevgenij87@gmail.com>
2025-12-05 22:49:54 +0200

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,42 @@
 'use strict';
 
-/**
- * @return {object}
- */
+//*
+//* @return {object}
+//*
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const obj = {
+    get result() {
+      return result;
+    },
+    add: (currentResult, number) => {
+      return currentResult + number;
+    },
+    subtract: (currentResult, number) => {
+      return currentResult - number;
+    },
+    multiply: (currentResult, number) => {
+      return currentResult * number;
+    },
+    divide: (currentResult, number) => {
+      return currentResult / number;
+    },
+    operate(cb, number) {
+      const newVal = cb(result, number);
+
+      result = newVal;
+
+      return obj;
+    },
+    reset: () => {
+      result = 0;
+
+      return obj;
+    },
+  };
+
+  return obj;
 }
 
 module.exports = makeCalculator;
