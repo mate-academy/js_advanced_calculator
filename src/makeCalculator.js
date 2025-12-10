081771464487cc16a2c88fa34c9ff15dca784003
Yevheniia Biliaieva <biliaieva99@gmail.com>
2025-12-10 17:23:32 +0200

    solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,42 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
-}
+  const calc = {
+    result: 0,
 
+    add: function (num) {
+      this.result += num;
+    },
+
+    subtract: function (num) {
+      this.result -= num;
+    },
+
+    multiply: function (num) {
+      this.result *= num;
+    },
+
+    divide: function (num) {
+      if (num === 0) {
+        return 'Sorry, division by zero is not allowed.';
+      }
+
+      this.result /= num;
+    },
+
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate: function (operation, value) {
+      operation.call(this, value);
+
+      return this;
+    },
+  };
+
+  return calc;
+}
 module.exports = makeCalculator;
