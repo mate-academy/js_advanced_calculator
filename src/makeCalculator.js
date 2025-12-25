347c5bad4a5c1837b4033fe95252d8723535a4e8
Vasyl Levitskyi <leva1990vale@gmail.com>
2025-12-25 09:28:08 +0200

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,35 +1,32 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
   return {
     result: 0,
 
-    add: function (a, b) {
-      return a + b;
-    },
-
-    subtract: function (a, b) {
-      return a - b;
-    },
-
-    multiply: function (a, b) {
-      return a * b;
-    },
-
-    divide: function (a, b) {
-      return a / b;
-    },
-
-    operate: function (callback, number) {
+    operate(callback, number) {
       this.result = callback(this.result, number);
 
       return this;
     },
 
-    reset: function () {
+    add(number) {
+      return this.operate((a, b) => a + b, number);
+    },
+
+    subtract(number) {
+      return this.operate((a, b) => a - b, number);
+    },
+
+    multiply(number) {
+      return this.operate((a, b) => a * b, number);
+    },
+
+    divide(number) {
+      return this.operate((a, b) => a / b, number);
+    },
+
+    reset() {
       this.result = 0;
 
       return this;
@@ -37,11 +34,4 @@ function makeCalculator() {
   };
 }
 
-const calculator = makeCalculator();
-
-calculator
-  .operate(calculator.add)
-  .reset()
-  .operate(calculator.subtract)
-  .operate(calculator.divide)
-  .operate(calculator.multiply);
+makeCalculator();
