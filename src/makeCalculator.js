2880c9487f496acbebbb4f74e49f20187f6a933c
Taras Borshch <tarasborshch4@gmail.com>
2026-01-24 14:17:29 +0200

    Implement makeCalculator func

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,51 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(number) {
+      this.result += number;
+    },
+
+    subtract(number) {
+      this.result -= number;
+    },
+
+    multiply(number) {
+      this.result *= number;
+    },
+
+    divide(number) {
+      this.result /= number;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, number) {
+      if (callback === this.add) {
+        this.add(number);
+      }
+
+      if (callback === this.subtract) {
+        this.subtract(number);
+      }
+
+      if (callback === this.multiply) {
+        this.multiply(number);
+      }
+
+      if (callback === this.divide) {
+        this.divide(number);
+      }
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
