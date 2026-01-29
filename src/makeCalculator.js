7fc2624597e05154fac4d60321f08cb47a0639e8
Taras Peteichuk <taraskull123@gmail.com>
2026-01-29 23:29:51 +0200

    added solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,46 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  return {
+    operate(operation, number) {
+      operation(number);
+
+      return this;
+    },
+    add(number) {
+      result += number;
+
+      return result;
+    },
+    subtract(number) {
+      result -= number;
+
+      return this;
+    },
+    multiply(number) {
+      result *= number;
+
+      return this;
+    },
+    divide(number) {
+      if (number === 0) {
+        throw new Error('Division by zero');
+      }
+      result /= number;
+
+      return this;
+    },
+    reset() {
+      result = 0;
+
+      return this;
+    },
+    get result() {
+      return result;
+    },
+  };
 }
 
 module.exports = makeCalculator;
