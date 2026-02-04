84c2ad0ef8175fae7dedae9b9afe7648d2e3f852
uselessmorning <shtoykoigor@ukr.net>
2026-02-04 18:14:35 +0100

    sollution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,26 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+
+    operate(action, value) {
+      this.result = action(this.result, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
