0d10baedd977f7db1cdcdd97d7ee31a55e5051a1
protocolo23 <narashikamaro27@gmail.com>
2026-03-12 07:12:07 -0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,67 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+      // eslint-disable-next-line padding-line-between-statements
+      return this;
+    },
+
+    subtract(value) {
+      this.result -= value;
+      // eslint-disable-next-line padding-line-between-statements
+      return this;
+    },
+
+    multiply(value) {
+      this.result *= value;
+      // eslint-disable-next-line padding-line-between-statements
+      return this;
+    },
+
+    divide(value) {
+      this.result /= value;
+      // eslint-disable-next-line padding-line-between-statements
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+      // eslint-disable-next-line padding-line-between-statements
+      return this;
+    },
+
+    operate(operation, value) {
+      operation.call(this, value);
+      // eslint-disable-next-line padding-line-between-statements
+      return this;
+    },
+  };
 }
+
+const calculator = makeCalculator();
+
+// Função auxiliar para imprimir sem usar console.log
+function printResult(label, value) {
+  process.stdout.write(`${label}: ${value}\n`);
+}
+
+calculator.operate(calculator.add, 21);
+printResult('Resultado 1', calculator.result); // 21
+
+calculator.reset();
+printResult('Resultado 2', calculator.result); // 0
+
+calculator
+  .operate(calculator.add, 10)
+  .reset()
+  .operate(calculator.subtract, 20)
+  .operate(calculator.multiply, 7)
+  .operate(calculator.divide, 5);
+
+printResult('Resultado final', calculator.result); // -28
 
 module.exports = makeCalculator;
