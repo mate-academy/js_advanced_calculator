8cbd79556e903b9bea7fe1c31e8b8eeafcb0940a
Michał Filip <biznesmf@gmail.com>
2026-03-16 18:46:43 +0100

    first commit

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,45 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(operation, x = 0) {
+      operation.call(this, x);
+
+      return this;
+    },
+
+    add(x) {
+      this.result += x;
+
+      return this;
+    },
+
+    subtract(x) {
+      this.result -= x;
+
+      return this;
+    },
+
+    multiply(x) {
+      this.result *= x;
+
+      return this;
+    },
+
+    divide(x) {
+      this.result /= x;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
