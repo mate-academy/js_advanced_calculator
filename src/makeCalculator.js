ea5b6031f6b2b17b252c36ff702a775c72cd9583
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-06 19:50:17 +0000

    feat: implement calculator functions

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,40 @@
 /**
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
-}
+const makeCalculator = () => {
+  const calculator = {
+    result: 0,
+
+    add(firstOperand, secondOperand) {
+      return firstOperand + secondOperand;
+    },
+
+    subtract(firstOperand, secondOperand) {
+      return firstOperand - secondOperand;
+    },
+
+    multiply(firstOperand, secondOperand) {
+      return firstOperand * secondOperand;
+    },
+
+    divide(firstOperand, secondOperand) {
+      return firstOperand / secondOperand;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(operation, operand) {
+      this.result = operation(this.result, operand);
+
+      return this;
+    },
+  };
+
+  return calculator;
+};
 
 module.exports = makeCalculator;
