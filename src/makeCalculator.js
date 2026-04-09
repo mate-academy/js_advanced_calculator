d9ec5a535635061ed58b3f2faa07af86787eb7d8
Cássia <cassiarsqueiroz@gmail.com>
2026-04-08 23:33:15 -0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+    add(current, value) {
+      return current + value;
+    },
+    subtract(current, value) {
+      return current - value;
+    },
+    multiply(current, value) {
+      return current * value;
+    },
+    divide(current, value) {
+      if (value === 0) {
+        throw new Error('Divisão por zero não é permitida');
+      }
+
+      return current / value;
+    },
+    operate(callback, number) {
+      const newResult = callback(this.result, number);
+
+      this.result = newResult;
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
