b269d24767b0df5ce301c836e55c05f2366fdf6a
Max Moroz <mirozmaksim1999@icloud.com.com>
2026-04-13 18:42:54 +0300

    change object name

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,7 @@
  * @return {object}
  */
 function makeCalculator() {
-  const obj = {
+  const calculator = {
     result: 0,
     operate(callback, number) {
       this.result = callback(this.result, number);
@@ -30,7 +30,7 @@ function makeCalculator() {
     },
   };
 
-  return obj;
+  return calculator;
 }
 
 module.exports = makeCalculator;
