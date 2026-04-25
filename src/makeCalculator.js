066feb11e8ed03ed8742375c10b57c49881d71ee
NazariiKaban <fedak_3efirka@ukr.net>
2026-04-25 20:42:06 +0200

    solution1

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,46 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(x) {
+      this.result += x;
+
+      return this;
+    },
+
+    subtract(x) {
+      this.result -= x;
+
+      return this;
+    },
+
+    divide(x) {
+      if (x === 0) {
+        throw new Error('Cannot divide on zero');
+      }
+      this.result /= x;
+
+      return this;
+    },
+
+    multiply(x) {
+      this.result *= x;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    operate(callback, num) {
+      return callback.call(this, num);
+    },
+  };
 }
 
 module.exports = makeCalculator;
