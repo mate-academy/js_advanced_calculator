0ad79a7c53b0b0bc922d04cb262de0a0e9f53628
George Peydy <greenstarmain2gmail.com>
2026-04-25 19:32:33 +0300

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calc = {
+    add(a) {
+      this.result += a;
+
+      return this;
+    },
+    subtract(a) {
+      this.result -= a;
+
+      return this;
+    },
+    multiply(a) {
+      this.result *= a;
+
+      return this;
+    },
+    divide(a) {
+      this.result /= a;
+
+      return this;
+    },
+    operate: function (operation, number) {
+      operation.call(this, number);
+
+      return this;
+    },
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+    result: 0,
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
