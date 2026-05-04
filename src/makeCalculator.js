45fcd3f3dea2468b91441a01cd7a19c44cba765c
VoloGor <goralvv@gmail.com>
2026-05-04 21:41:26 +0200

    Make Calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,38 @@
  */
 function makeCalculator() {
   // write code here
+  let result = 0;
+
+  const add = (current, number) => current + number;
+  const sub = (current, number) => current - number;
+  const mul = (current, number) => current * number;
+  const div = (current, number) => current / number;
+
+  const operate = (callback, number) => {
+    result = callback(result, number);
+    calculator.result = result;
+
+    return calculator;
+  };
+
+  const reset = () => {
+    result = 0;
+    calculator.result = result;
+
+    return calculator;
+  };
+
+  const calculator = {
+    result: 0,
+    add,
+    sub,
+    mul,
+    div,
+    operate,
+    reset,
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
