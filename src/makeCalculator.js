6aa7f60318c606b50bfd0c94ca65955c2355b898
Oleksandr Kostiushko <a.v.bum25@gmail.com>
2026-06-03 16:09:08 +0300

    add solution 03_06_2026

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,40 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    // початковий стан калькулятора
+    result: 0,
+    // callback, бере поточний результат, додає до нього число і повертає суму
+    add(currentResult, num) {
+      return currentResult + num;
+    },
+
+    subtract(currentResult, num) {
+      return currentResult - num;
+    },
+
+    divide(currentResult, num) {
+      return currentResult / num;
+    },
+
+    multiply(currentResult, num) {
+      return currentResult * num;
+    },
+
+    // головна дія. Викликає callback, міняє result і повертає увесь об'єкт
+    operate(callback, num) {
+      this.result = callback(this.result, num);
+
+      return this; // повертаємо об'єкт для ланцюжка
+    },
+
+    // метод скидання
+    reset() {
+      this.result = 0;
+
+      return this; // повертаємо об'єкт для ланцюжка
+    },
+  };
 }
 
 module.exports = makeCalculator;
