159761c05e30493ca98a9a2d0002a8b411af6416
Vladimir Kolosov <12vova1212ukr@gmail.com>
2026-06-04 18:12:14 +0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    add: (value) => {
+      calculator.result += value;
+
+      return calculator.result;
+    },
+    subtract: (value) => {
+      calculator.result -= value;
+
+      return calculator.result;
+    },
+    multiply: (value) => {
+      calculator.result *= value;
+
+      return calculator.result;
+    },
+    divide: (value) => {
+      calculator.result /= value;
+
+      return calculator.result;
+    },
+    operate(operationFn, value) {
+      operationFn(value);
+
+      return calculator;
+    },
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
