9386d66a7ba9b71d5781638fb0d8dd55e3765c26
Skoulskiy <ogurethcs@gmail.com>
2026-06-19 11:24:43 +0300

    Add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,6 +4,29 @@
  * @return {object}
  */
 function makeCalculator() {
+  let currentResult = 0;
+
+  return {
+    get result() {
+      return currentResult;
+    },
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+
+    operate(callback, num) {
+      currentResult = callback(currentResult, num);
+
+      return this;
+    },
+
+    reset() {
+      currentResult = 0;
+
+      return this;
+    },
+  };
   // write code here
 }
 
