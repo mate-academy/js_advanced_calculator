694478ce9cd5599d49d2b2450420a82b9c1dc773
Andrii Yarmolenko <andrii.yarmolenko88@gmail.com>
2026-07-10 16:32:48 +0300

    done

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,31 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+    add(num) {
+      this.result += num;
+    },
+    subtract(num) {
+      this.result -= num;
+    },
+    multiply(num) {
+      this.result = this.result * num;
+    },
+    divide(num) {
+      this.result = this.result / num;
+    },
+    operate(callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
