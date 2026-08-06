b650eb99afa2170d7d7e7b41e045bc6cf002c047
Anastasi Bielikova <anastasiabelikova39@gmail.com>
2026-08-06 12:20:19 +0300

    add

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,28 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calc = {};
+
+  calc.result = 0;
+
+  calc.add = (a, b) => a + b;
+  calc.subtract = (a, b) => a - b;
+  calc.multiply = (a, b) => a * b;
+  calc.divide = (a, b) => a / b;
+
+  calc.reset = () => {
+    calc.result = 0;
+
+    return calc;
+  };
+
+  calc.operate = (callback, number) => {
+    calc.result = callback(calc.result, number);
+
+    return calc;
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
