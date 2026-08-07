4242982394eadf09141018ad687bc7df754a8324
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-07 19:11:33 +0300

    Calculator developed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    operate(operation, amount) {
+      this.result = operation(this.result, amount);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+
+    add(value1, value2) {
+      return value1 + value2;
+    },
+
+    subtract(value1, value2) {
+      return value1 - value2;
+    },
+
+    multiply(value1, value2) {
+      return value1 * value2;
+    },
+
+    divide(value1, value2) {
+      return value1 / value2;
+    },
+  };
 }
+
+const calculator = makeCalculator();
+
+calculator.operate(calculator.subtract, 20);
 
 module.exports = makeCalculator;
