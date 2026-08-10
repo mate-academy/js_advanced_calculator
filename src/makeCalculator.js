a74d1b9acf6e9a4ea3364955b377a1812cd53b12
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-08-10 12:23:20 +0300

    Completed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,39 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calc = {
+    result: 0,
+
+    add(value) {
+      return this.result + value;
+    },
+
+    subtract(value) {
+      return this.result - value;
+    },
+
+    multiply(value) {
+      return this.result * value;
+    },
+
+    divide(value) {
+      return this.result / value;
+    },
+
+    operate(callback, value) {
+      this.result = callback.call(this, value);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calc;
 }
 
 module.exports = makeCalculator;
