9fa5ead5d56c8670d069b2d401773d1dc02bb78b
PavloMolytovnyk <pavlomolytovnyk@icloud.com>
2026-08-14 15:35:06 +0300

    Change 1

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,32 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  return {
+    result: 0,
+
+    add(num) {
+      this.result += num;
+    },
+    subtract(num) {
+      this.result -= num;
+    },
+    multiply(num) {
+      this.result *= num;
+    },
+    divide(num) {
+      this.result /= num;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(callback, number) {
+      callback.call(this, number);
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
