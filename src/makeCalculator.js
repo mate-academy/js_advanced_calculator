91608d9d586a5ffdd3eb92a6943fc4cda88f9dbf
Ryahn Carvalho <cavralhoryahn@gmail.com>
2026-08-19 23:40:45 -0300

    added advanced calculator task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,35 @@
 /**
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
-}
+const makeCalculator = () => {
+  return {
+    result: 0,
+
+    add(num) {
+      this.result += num;
+    },
+    subtract(num) {
+      this.result -= num;
+    },
+    multiply(num) {
+      this.result *= num;
+    },
+    divide(num) {
+      this.result /= num;
+    },
+
+    operate(callback, num) {
+      callback.call(this, num);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+};
 
 module.exports = makeCalculator;
